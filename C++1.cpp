5ef73eb6855afd1c0836cf26f53ce1495951f741
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-21 17:05:01 +0800

    20.比较运算符

--- C++1.cpp
+++ C++1.cpp
@@ -3,38 +3,33 @@ using namespace std;
 
 int main() {
 
-	//赋值运算符
-	// = += -= *= %=
+	//比较运算符
+	// == != < > <= >=
+	//返回1为真，返回0为假
 
-	//=
 	int a1 = 10;
-	a1 = 100;
-	cout << "a1=" << a1 << endl;
+	int b1 = 20;
+	cout << (a1 == b1) << endl;
 
-	//+=
 	int a2 = 10;
-	a2 +=2;//a=a+2
-	cout << "a2=" << a2 << endl;
+	int b2 = 20;
+	cout << (a2 != b2) << endl;
 
-	//-=
 	int a3 = 10;
-	a3 -= 2;//a=a-2
-	cout << "a3=" << a3 << endl;
+	int b3 = 20;
+	cout << (a3 < b3) << endl;
 
-	//*=
 	int a4 = 10;
-	a4 *= 2;//a=a*2
-	cout << "a4=" << a4 << endl;
+	int b4 = 20;
+	cout << (a4 > b4) << endl;
 
-	//*=
 	int a5 = 10;
-	a5 /= 2;//a=a/2
-	cout << "a5=" << a5 << endl;
+	int b5 = 20;
+	cout << (a5 <= b5) << endl;
 
-	//*=
 	int a6 = 10;
-	a6 %= 2;//a=a%2
-	cout << "a6=" << a6 << endl;
+	int b6 = 20;
+	cout << (a6 >= b6) << endl;
 
 	system("pause");
 
