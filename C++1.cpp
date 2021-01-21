84460116d2811e339345148a7385e6a94233c76e
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-21 23:04:21 +0800

    选择结构-switch语句

--- C++1.cpp
+++ C++1.cpp
@@ -3,21 +3,46 @@ using namespace std;
 
 int main() {
 	
-	//三目运算符  表达式1？ 表达式2：表达式3
-	//意思是判断表达式1 是否为真，则返回表达式2，否则返回表达式3
+	//switch 语句  执行多条件分支语句
+	//给一个电影打分
+	//10~9经典
+	//7~8 非常好
+	//5~6 一般
+	//5分一下  烂片
 
-	//创建三个变量 a b c 
-	//将a和b做比较，将变量大的值赋值给c
+	//步骤
+	//1.邀请用户给电影评分
+	cout << "请给该电影评分：";
+	int score;
+	cin >> score;
+	cout << "您给该电影评分为：" << score <<"分"<< endl;
 
-	int a = 10;
-	int b = 20;
-	int c = 0;
-	c = (a > b ? a : b);
-	cout << "c=" << c << endl;
+	//2.根据用户打分给电影评等级
 
-	(a < b ? a : b) = 100;//三目运算符返回的是变量，可以继续赋值
-	cout << "a=" << a << endl;
-	cout << "b=" << b << endl;
+	switch (score)//区别于if if后面可以判断表达式，可以判断一个区间，但是switch只能是整型或者字符型，但是switch结构清晰，执行效率高
+	{	
+	case 10:
+		cout << "您认为该电影为经典电影" << endl; 
+		break;//退出当前分数
+	case 9:
+		cout << "您认为该电影为经典电影" << endl;
+		break;//退出当前分数
+	case 8:
+		cout << "您认为该电影为好电影" << endl;
+		break;//退出当前分数
+	case 7:
+		cout << "您认为该电影为好电影" << endl;
+		break;//退出当前分数
+	case 6:
+		cout << "您认为该电影为一般电影" << endl;
+		break;//退出当前分数
+	case 5:
+		cout << "您认为该电影为一般电影" << endl;
+		break;//退出当前分数
+	default:
+		cout << "您认为该电影为烂片" << endl;
+		break;//退出当前分数
+	}
 
 	system("pause");
 
