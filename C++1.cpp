a1454ed5e5284e519ee8547562bdd98c790ecdb0
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-21 16:42:00 +0800

    18 取模运算

--- C++1.cpp
+++ C++1.cpp
@@ -3,23 +3,23 @@ using namespace std;
 
 int main() {
 	
-	//加减乘除
+	// %取模（余数）
 	int a1 = 10;
 	int b1 = 3;
+	cout << a1 % b1 << endl;
 
-	cout << a1 + b1 << endl;
-	cout << a1 - b1 << endl;
-	cout << a1 * b1 << endl;
-	cout << a1 / b1 << endl;//两个整数做相除的运算，结果依然是整数，去除小数部分
+	int a2 = 10;
+	int b2 = 20;
+	cout << a2 % b2 << endl;
 
-	//int a2 = 10;
-	//int b2 = 0;
-	//cout << a2 / b2 << endl;//除数不可以为0
+	//int a3 = 10;
+	//int b3 = 0;
+	//cout << a3 % b3 << endl; //取模运算是基于除法运算，除数为0的时候不可以做取模运算
 
-	double d1 = 0.5;
-	double d2 = 0.2;
-	cout << d1 / d2 << endl;//小数也可以相除，除的尽的时候是整数，除不尽的时候是小数
-	
+	//double d1 = 3.14;
+	//double d2 = 1.5;
+	//cout << d1 % d2 << endl;//C++中小数不可以做取模运算
+
 
 	system("pause");
 
