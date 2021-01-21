2db6c9c6d6ba5986d778f97453432c952301e64c
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-21 16:59:05 +0800

    赋值运算符

--- C++1.cpp
+++ C++1.cpp
@@ -3,27 +3,38 @@ using namespace std;
 
 int main() {
 
-	//1.前置递增
-	//2.后置递增
-	//3.前置和后置的区别
+	//赋值运算符
+	// = += -= *= %=
 
-	int a = 10;
-	++a;//让变量+1
-	cout <<"a="<<a << endl;
-	
-	int	b = 10;
-	b++;//让变量+1
-	cout << "b="<<b << endl;
-
+	//=
 	int a1 = 10;
-	int b1 = ++a1 * 10;//前置运算，先让变量进行+1，再进行表达式运算
-	cout << "a1=" << a1 << endl;//这里的a1进行了++运算
-	cout << "b1=" << b1 << endl;
+	a1 = 100;
+	cout << "a1=" << a1 << endl;
 
+	//+=
 	int a2 = 10;
-	int b2 = a2++ * 10;//后置运算 ，先进行表达式运算，再进行++
-	cout << "a2=" << a2 << endl;//这里的a2进行了++运算
-	cout << "b2=" << b2 << endl;
+	a2 +=2;//a=a+2
+	cout << "a2=" << a2 << endl;
+
+	//-=
+	int a3 = 10;
+	a3 -= 2;//a=a-2
+	cout << "a3=" << a3 << endl;
+
+	//*=
+	int a4 = 10;
+	a4 *= 2;//a=a*2
+	cout << "a4=" << a4 << endl;
+
+	//*=
+	int a5 = 10;
+	a5 /= 2;//a=a/2
+	cout << "a5=" << a5 << endl;
+
+	//*=
+	int a6 = 10;
+	a6 %= 2;//a=a%2
+	cout << "a6=" << a6 << endl;
 
 	system("pause");
 
