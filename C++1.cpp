f75a1c514159fa04c8c2dcaeee8e403eda36c30e
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-21 16:34:37 +0800

    17-运算符-加减乘除

--- C++1.cpp
+++ C++1.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
 using namespace std;
-int main() 
-{
-    cout << "Hello c++!" << endl; 
-    return 0; 
+
+int main() {
+	
+	//加减乘除
+	int a1 = 10;
+	int b1 = 3;
+
+	cout << a1 + b1 << endl;
+	cout << a1 - b1 << endl;
+	cout << a1 * b1 << endl;
+	cout << a1 / b1 << endl;//两个整数做相除的运算，结果依然是整数，去除小数部分
+
+	//int a2 = 10;
+	//int b2 = 0;
+	//cout << a2 / b2 << endl;//除数不可以为0
+
+	double d1 = 0.5;
+	double d2 = 0.2;
+	cout << d1 / d2 << endl;//小数也可以相除，除的尽的时候是整数，除不尽的时候是小数
+	
+
+	system("pause");
+
+	return 0;
+
 }
