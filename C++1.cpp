d0ed833d5673b24b2a336041c7129575ef857902
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-21 23:27:52 +0800

    循环语句（while）猜数字游戏

--- C++1.cpp
+++ C++1.cpp
@@ -1,47 +1,38 @@
 #include<iostream>
 using namespace std;
-
-int main() {
+#include<ctime>//系统时间头文件
+int main() 
+{
 	
-	//switch 语句  执行多条件分支语句
-	//给一个电影打分
-	//10~9经典
-	//7~8 非常好
-	//5~6 一般
-	//5分一下  烂片
+	//猜数字游戏，猜一个数字和随机数字比较
+	//猜对了退出游戏
+	//猜错了，提示结果过大or过小，再猜一次
 
-	//步骤
-	//1.邀请用户给电影评分
-	cout << "请给该电影评分：";
-	int score;
-	cin >> score;
-	cout << "您给该电影评分为：" << score <<"分"<< endl;
+	//添加随机数种子 利用当前系统时间生成随机数，防止每次随机数都一样
+	srand((unsigned int)time(NULL));
+	//1.系统生成随机数
+	int num = rand() % 100 + 1;//生成0-99的随机数
 
-	//2.根据用户打分给电影评等级
+	//2.玩家输入的数据
+	int value = 0;
+	while (1)
+	{
+		cin >> value;
+		if (value > num)
+		{
+			cout << "您猜测的数据过大" << endl;
 
-	switch (score)//区别于if if后面可以判断表达式，可以判断一个区间，但是switch只能是整型或者字符型，但是switch结构清晰，执行效率高
-	{	
-	case 10:
-		cout << "您认为该电影为经典电影" << endl; 
-		break;//退出当前分数
-	case 9:
-		cout << "您认为该电影为经典电影" << endl;
-		break;//退出当前分数
-	case 8:
-		cout << "您认为该电影为好电影" << endl;
-		break;//退出当前分数
-	case 7:
-		cout << "您认为该电影为好电影" << endl;
-		break;//退出当前分数
-	case 6:
-		cout << "您认为该电影为一般电影" << endl;
-		break;//退出当前分数
-	case 5:
-		cout << "您认为该电影为一般电影" << endl;
-		break;//退出当前分数
-	default:
-		cout << "您认为该电影为烂片" << endl;
-		break;//退出当前分数
+		}
+		else if (value < num)
+		{
+			cout << "您猜测的数据过小" << endl;
+
+		}
+		else if (value = num)
+		{
+			cout << "恭喜您猜对了" << endl;
+			break;
+		}
 	}
 
 	system("pause");
