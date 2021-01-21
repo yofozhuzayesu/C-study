919c6428013c0a514422161367ca39b53df090ae
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-21 17:17:24 +0800

    逻辑运算符（与或非）

--- C++1.cpp
+++ C++1.cpp
@@ -3,33 +3,42 @@ using namespace std;
 
 int main() {
 
-	//比较运算符
-	// == != < > <= >=
-	//返回1为真，返回0为假
+	//逻辑运算符
+	//或 ||  其中一个为真则为真，两个都为假则假
+	//与 &&  两个都为真的时候才为真，否则为假
+	//非 ！  a假，则!a为真
 
+	//非
+	int a = 10;//只要不为0，则都为真
+	cout << a << endl;//输出10
+	cout << !a << endl;//输出假
+	cout << !!a << endl;//输出真
+
+	//与
 	int a1 = 10;
-	int b1 = 20;
-	cout << (a1 == b1) << endl;
+	int b1 = 10;
+	cout << (a1&&b1) << endl;//两个都为真输出1；
 
 	int a2 = 10;
-	int b2 = 20;
-	cout << (a2 != b2) << endl;
+	int b2 = 0;
+	cout << (a2&&b2) << endl;//其中一个为假，输出0
 
-	int a3 = 10;
-	int b3 = 20;
-	cout << (a3 < b3) << endl;
+	int a3 = 0;
+	int b3 = 0;
+	cout << (a3&&b3) << endl;//两个都为假，输出0
+ 
+	//或
+	int c1 = 10;
+	int d1 = 10;
+	cout << (c1||d1) << endl;//两个都为真，输出1
 
-	int a4 = 10;
-	int b4 = 20;
-	cout << (a4 > b4) << endl;
+	int c2 = 10;
+	int d2 = 0;
+	cout << (c2||d2) << endl;//其中一个为假一个为真，输出1
 
-	int a5 = 10;
-	int b5 = 20;
-	cout << (a5 <= b5) << endl;
-
-	int a6 = 10;
-	int b6 = 20;
-	cout << (a6 >= b6) << endl;
+	int c3 = 0;
+	int d3 = 0;
+	cout << (c3||d3) << endl;//两个都为假，输出0
 
 	system("pause");
 
