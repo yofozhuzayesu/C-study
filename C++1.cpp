d9bd9a9ef4229190bf4c361e32813a03dbc0bee3
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-22 21:21:45 +0800

    二维数组的应用-成绩统计

--- C++1.cpp
+++ C++1.cpp
@@ -1,40 +1,28 @@
 #include<iostream>
+#include<string>
+
 using namespace std;
 
 int main()
 {
-	//二维数组数组名
-	/*
-	1.查看二维数组所占内存空间
-	2.获取二维数组的首地址
-	*/
-
-	//查看二维数组的内存空间
-	int arr[2][3] = {
-		{1,2,3},
-		{4,5,6}
+	//二维数组应用
+	//有三名同学的语数外成绩，输出三名同学的成绩
+	int score[3][3] = {
+		{100,100,100},
+		{90,20,60},
+		{60,50,90}
 	};
-	cout << "二维数组所占空间大小=" << sizeof(arr) << endl;
+	string names[3] = { "张三","李四","王五" };//需要调用string头文件
+	for (int i = 0; i < 3; i++)
+	{
+		int sum = 0;
+		for (int j = 0; j < 3; j++)
+		{
+			sum += score[i][j];
+		}
+		cout <<names[i]<<"的总成绩为："<<sum << endl;
 
-	cout << "二维数组第一行占用内存：" << sizeof(arr[0]) << endl;//sizeof只能计算行占用的内存，不能用sizeof[][0]计算列的内存
-
-	cout << "二维数组第一个元素占用的内存：" << sizeof(arr[0][0]) << endl;
-
-	cout << "二维数组的行数：" << sizeof(arr) / sizeof(arr[0]) << endl;//计算行数用总的内存除以一行占用的内存
-
-	cout << "二维数组的列数：" << sizeof(arr[0]) / sizeof(arr[0][0]) << endl;//计算列数用一行的内存空间，除以一个元素的内存空间
-
-	//查看二维数组的首地址
-	cout << "二维数组的首地址为："<<(int)arr << endl;
-
-	//查看二维数组第一行的首地址
-	cout << "二维数组第一行的首地址为：" << (int)arr[0] << endl;
-
-	//查看第二行的首地址
-	cout << "二维数组第二行的首地址为：" << (int)arr[1]<< endl;
-
-	//查看第一个元素的首地址
-	cout << "二维数组第一个元素的首地址为：" << (int)&arr[0][0] << endl;//&——取地址符
+	}
 	system("pause");
 	return 0;
 
