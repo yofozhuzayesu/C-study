074a3ec9b54d330266101bce2e3d4e8b8f24c202
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-22 15:08:26 +0800

    嵌套循环（for）实现乘法表

--- C++1.cpp
+++ C++1.cpp
@@ -3,28 +3,18 @@ using namespace std;
 
 int main() 
 { 
-	//for循环语句
-	//语法 for (起始表达式;条件表达式;末尾表达式）
-	//｛
-	//   循环语句
-	//	｝
+	
+	//嵌套循环实现99乘法表
 
-	for (int i = 1; i <= 100; i++)
+	for (int i = 1; i <= 9; i++)//外层循环
 	{
-		int a = i % 10;
-		int b = i / 10;
-		if (a==7||b==7||i%7==0)
+		for (int j = 1; j <=i; j++)//内层循环
 		{
-			cout << "敲桌子" << endl;
+			cout << j << "*" << i << "=" << i * j<<"\t";
+		}
+		cout << endl;
 
-		}
-		else
-		{
-			cout << i << endl;
-		}
-		
 	}
-
 	system("pause");
 	return 0;
 
