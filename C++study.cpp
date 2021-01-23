e5be6a82f1f4858b00374669875aa8bf5ef82437
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-23 21:02:13 +0800

    指针和数组
    利用指针遍历数组

--- C++study.cpp
+++ C++study.cpp
@@ -2,33 +2,29 @@
 #include<string>
 using namespace std;
 
-//const 修饰指针
-/*
-1.const修饰指针 常量指针：指针的指向可以改，但是指针指向的值不可以改 (const int *p)
-2.const修饰常量 指针常量：指针的指向不可以改，但是指针指向的值可以改(int * const p )
-3.const即修饰指针，又修饰常量：指针的指向和指向的值都不可以改（const int * const p ）
-*/
 
+//指针和数组
+//利用指针访问数组的元素
 
 int main()
 {
-	//const修饰指针
-	int a = 20;
-	int b = 10;
-	const int * p = &a;
-	p = &b;
-	//*p = b;错误，指针指向的值不可以改
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int * p = arr;//arr就是数组的首地址
+	cout << "数组的第一个元素为：" << arr[0] << endl;
 
-	//const修饰常量
-	int * const p2 = &a;
-	*p2 = b;
-	//p2 = &b;错误，指针的指向不可以修改
+	cout << "利用指针访问第一个元素：" << *p << endl;
 
-	//const双修饰
-	const int * const p3 = &a;
-	//*p3 = b;错误
-	//p3 = &b;错误
+	cout << "利用指针访问第二个元素：" << *p+2 << endl;//指针的加法是每次+4
 
+	//利用指针遍历数组
+	int *p2 = arr;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << *p2 << endl;
+		p2++;
+
+	}
 	system("pause");
 	return 0;
 }
