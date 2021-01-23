7f6699e4348d54c1c1547756a9a257fc917c3a29
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-23 23:26:16 +0800

    指针配合函数和数组实现冒泡排序
    注意数组作为参数传递的时候，传递的是数组的首地址，以及传递地址的时候会修饰形参

--- C++study.cpp
+++ C++study.cpp
@@ -3,26 +3,44 @@
 using namespace std;
 
 
-//指针和函数
-//1.值传递
-//2.地址传递
+//指针、数组、函数
+//封装一个函数，利用冒泡排序，实现对整形数组的升序排序
 
-void swap(int *p1, int *p2)//地址传递
+//参数1 数组的首地址 参数2 数组长度
+void BubbleSort(int arr[],int len)//参数是数组时，传递的是数组的首地址，在函数中无法计算数组长度，需要在外部计算
 {
-	int temp = *p1;
-	*p1 = *p2;
-	*p2 = temp;
-	cout << "swap a=" << *p1 << endl;
-	cout << "swap b=" << *p2 << endl;
-
+	for (int i = 0; i < len-1; i++)
+	{
+		int * p = arr;//将指针重新定位到数组头部
+		for (int j = 0; j < len- i-1; j++)
+		{	
+			if (*p > *(p + 1))//*的优先级比+高，所以要加括号，以下同理
+			{
+				int temp = *p;
+				*p = *(p + 1);
+				*(p + 1) = temp;
+			}
+			p++;
+		}
+	}
+	int *p2 = arr;//for循环中的变量作用域只在for内，因此要重新定义一个指针
+	for (int i = 0; i < len; i++)
+	{
+		cout << *p2 << endl;
+		p2++;
+	}
 }
+
+
 int main()
 {
-	int a = 10;
-	int b = 20;
-	swap(&a, &b);//地址传递可以修饰实参
-	cout << "a=" << a << endl;
-	cout << "b=" << b << endl;
+	int arr2[] = {5,4,6,8,2,3,7,5,8,6,9,4,5,2,6};
+	int length = sizeof(arr2) / sizeof(arr2[0]);
+	BubbleSort(arr2,length);
+	for (int i = 0; i < length; i++)
+	{
+		cout << arr2[i];//输出的也是排序之后的，说明地址传递会修饰实参
+	}
 	system("pause");
 	return 0;
 }
