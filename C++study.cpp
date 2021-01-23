cc17005623412fbfb9cfec178a350b54b568501b
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-23 20:24:49 +0800

    指针变量占用的内存空间

--- C++study.cpp
+++ C++study.cpp
@@ -1,34 +1,21 @@
 #include<iostream>
 #include<string>
-#include"swap.h"//注意这里用的是引号，不是尖括号
 using namespace std;
 
-//指针
-//基本概念：通过指针间接访问内存
-/*
-1.内存编号从0开始，一般用16进制标书
-2.可以利用指针变量记录地址
-*/
+//指针占用内存空间
+//在32位操作系统下：指针变量占4个字节的内存空间(无论什么类型的指针，float double char string)
+//64位：8个字节  即x64
+
 
 int main()
 {
-	//定义指针
 	int a = 10;
-	//定义指针的语法：数据类型 * 指针变量
-	int * p;
-	//让指针记录变量a的地址
-	p = &a;
-	cout << "a的地址为：" << &a << endl;
-	cout << "指针p=" << p << endl;
-
-	//2.使用指针
-	//可以通过解引用的方式来找到指针指向的内存
-	//指针前加*表示解引用，找到指针指向的内存中的数据
-	*p = 100;
-	cout << "a=" << a << endl;
-	cout << "*p=" << *p << endl;
-
+	int *p = &a;
+	cout << "指针占用的内存空间=" << sizeof(float * ) << endl;
+	cout << "指针占用的内存空间=" << sizeof(int *) << endl;
+	cout << "指针占用的内存空间=" << sizeof(double *) << endl;
+	cout << "指针占用的内存空间=" << sizeof(char *) << endl;
+	cout << "指针占用的内存空间=" << sizeof(string *) << endl;
 	system("pause");
 	return 0;
 }
-
