5f317a17cd5ac6822e3849a37ddf7e9e112ba277
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-25 17:48:15 +0800

    结构体指针
    结构体指针访问结构体元素，符号是->

--- C++study.cpp
+++ C++study.cpp
@@ -2,13 +2,8 @@
 #include<string>
 using namespace std;
 
+//结构体指针，利用操作符->可以通过结构体指针访问结构体属性
 
-//结构体数组：将自定义的结构体放入数组中方便维护
-//语法 struct 结构体名 数组名[元素个数] = {{},{},{},...{}}；
-
-//结构体除了定义结构体可以在main之外，定义和赋值需要在main函数之内做
-
-//1.定义结构体
 struct student
 {
 	string name;
@@ -19,22 +14,17 @@ struct student
 
 int main()
 {
-	//2.创建结构体数组
-	student arr[2]
-	{
-		{"张三",18,65},
-		{"李四",17,95}
-	};
-	//3.给结构体数组中的元素赋值
-	arr[0].name = "王五";//把结构体数组中的第一个元素中的姓名更改
-	
-	//遍历结构体数组
-	for (int i = 0; i < 2; i++)
-	{
-		cout << arr[i].name << endl;
-		cout << arr[i].age << endl;
-		cout << arr[i].grade << endl;
-	}
+	//创建学生结构体变量
+	student s1 = { "张三",18,65 };
+
+	//通过指针指向结构体变量
+	student * p = &s1;
+
+	//通过指针访问结构体变量中的数据,->
+	cout << p->name << endl;
+	cout << p->age << endl;
+	cout << p->grade << endl;
+
 
 	system("pause");
 	return 0;
