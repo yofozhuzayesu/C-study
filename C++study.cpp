97b9a4eeee1c82d521b905b7fe7f6eabbd667789
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-26 00:24:05 +0800

    结构体例2-冒泡排序
    数组作为参数传递时，传递的是地址
    结构体冒泡排序，交换的单元是结构体变量，而不是结构体变量中的一个组成部分

--- C++study.cpp
+++ C++study.cpp
@@ -1,65 +1,71 @@
 #include<iostream>
 #include<string>
-#include<ctime>
 using namespace std;
 
-
-//案例1 
+//案例2
 /*
-1.每名老师带5个学生，共有三名老师
-2.老师的结构体中-老师姓名和存放5名学生的数组作为成员
-3.学生的成员有姓名，考试分数，
-4.创建数组存放三名老师，通过函数给每个老师以及所带的学生赋值，最终打印出老师的数据及老师所带的学生数据
-*/
-struct student 
+  设计一个英雄的结构体：姓名 年龄 性别  共5名英雄
+  通过冒泡排序的方法，将数组中的英雄按照年龄进行升序排序，最终打印排序后的结果
+ */
+
+//英雄结构体
+struct Hero
 {
 	string name;
-	int score;
+	int age;
+	string sex;
 };
 
-struct teacher
+//英雄赋值
+void allocate(Hero h[], int len)
 {
-	string name;
-	student stu[5];
-};
-void allocate(teacher t[],int length)//给老师和学生赋值的函数
-{
-	string NameSeed = "ABCDE";//设置一个种子，方便赋值
-	for (int i = 0; i < length; i++)//给老师赋值
+	string NameSeed[5] = { "刘备","关羽","张飞","赵云","貂蝉" };
+	string SexSeed[5] = { "男","男","男","男","女" };
+	int a[5] = { 23,22,20,21,19 };
+	for (int i = 0; i < len; i++)
 	{
-		t[i].name = "Teacher_";
-		t[i].name += NameSeed[i];
-		for (int j = 0; j < 5; j++)//通过内层循环给学生赋值
+		h[i].name = NameSeed[i];
+		h[i].sex = SexSeed[i];
+		h[i].age = a[i];
+	}
+}
+
+//冒泡排序
+void BubbleSort( Hero h[], int len)
+{
+	for (int i = 0; i < len-1; i++)
+	{
+		for (int j = 0; j < len-i-1; j++)
 		{
-			t[i].stu[j].name = "Student_";
-			t[i].stu[j].name += NameSeed[j];
-			t[i].stu[j].score = rand()%61+40;
+			//如果j下标的元素大于 j+1元素的年龄，就交换两个元素
+			if (h[j].age>h[j+1].age)
+			{
+				Hero temp = h[j];//结构体冒泡排序，交换的单元是结构体变量，而不是结构体变量中的一个组成部分
+				h[j] = h[j + 1];
+				h[j + 1] = temp;
+			}
 		}
 	}
 }
 
-void print(teacher t[], int length)//输出成绩
+void print(Hero h[], int len)
 {
-	for (int i = 0; i < length; i++)
+	for (int  i = 0; i < len; i++)
 	{
-		cout << "老师姓名为：" << t[i].name << endl;
-		for (int j = 0; j < 5; j++)
-		{
-			cout << "\t" << t[i].stu[j].name << "成绩为：" << t[i].stu[j].score << endl;;
-		}
+		cout << h[i].name << "  " << h[i].age << "  " << h[i].sex << endl;
 	}
 }
+
 int main()
 {
-	//随机数种子
-	srand((unsigned int)time(NULL));
-	//创建三名老师的数组
-	teacher te[3];
-	//通过函数赋值
-	int len = sizeof(te) / sizeof(te[0]);
-	allocate(te, len);
-	//输出学生成绩
-	print(te, len);
+	Hero h1[5];//创建结构体变量
+	int length = sizeof(h1) / sizeof(h1[0]);//计算结构体数组长度
+	//结构体元素赋值
+	allocate(h1, length);
+	//冒泡排序
+	BubbleSort(h1,length);
+	//输出
+	print(h1, length);
 
 	system("pause");
 	return 0;
