5eb28006dcc284ec54be1525c286315179990409
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-25 23:29:47 +0800

    结构体案例1-嵌套结构体
    数组作为变量参数时的传递方法
    随机数 srand

--- C++study.cpp
+++ C++study.cpp
@@ -1,34 +1,65 @@
 #include<iostream>
 #include<string>
+#include<ctime>
 using namespace std;
 
-//结构体中用const防止误操作
-//const使用场景——地址传递
 
-struct student
+//案例1 
+/*
+1.每名老师带5个学生，共有三名老师
+2.老师的结构体中-老师姓名和存放5名学生的数组作为成员
+3.学生的成员有姓名，考试分数，
+4.创建数组存放三名老师，通过函数给每个老师以及所带的学生赋值，最终打印出老师的数据及老师所带的学生数据
+*/
+struct student 
 {
 	string name;
-	int age;
-	int grade;
+	int score;
 };
 
-//打印结构体名称
-//采用地址传递方式，可以减少内存空间，并且不会复制新的副本出来
-void PrintStudents(const student *s)
+struct teacher
 {
-	//s->age = 100;加入const之后，一旦有修改的操作就会报错，防止我们的误操作
-	cout << s->name << endl;
-	cout << s->age << endl;
-	cout << s->grade << endl;
-
+	string name;
+	student stu[5];
+};
+void allocate(teacher t[],int length)//给老师和学生赋值的函数
+{
+	string NameSeed = "ABCDE";//设置一个种子，方便赋值
+	for (int i = 0; i < length; i++)//给老师赋值
+	{
+		t[i].name = "Teacher_";
+		t[i].name += NameSeed[i];
+		for (int j = 0; j < 5; j++)//通过内层循环给学生赋值
+		{
+			t[i].stu[j].name = "Student_";
+			t[i].stu[j].name += NameSeed[j];
+			t[i].stu[j].score = rand()%61+40;
+		}
+	}
 }
 
+void print(teacher t[], int length)//输出成绩
+{
+	for (int i = 0; i < length; i++)
+	{
+		cout << "老师姓名为：" << t[i].name << endl;
+		for (int j = 0; j < 5; j++)
+		{
+			cout << "\t" << t[i].stu[j].name << "成绩为：" << t[i].stu[j].score << endl;;
+		}
+	}
+}
 int main()
 {
-	//创建结构体变量
-	student s1 = { "张三",15,60 };
-	//通过函数打印结构体变量信息
-	PrintStudents(&s1);//
+	//随机数种子
+	srand((unsigned int)time(NULL));
+	//创建三名老师的数组
+	teacher te[3];
+	//通过函数赋值
+	int len = sizeof(te) / sizeof(te[0]);
+	allocate(te, len);
+	//输出学生成绩
+	print(te, len);
 
 	system("pause");
 	return 0;
