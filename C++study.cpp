e736c925ccfc5a81613048a1a553826339549ecd
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-25 17:43:11 +0800

    结构体数组
    定义结构体的时候需要在main函数之外，其余的创建结构体变量，赋值均需要在main函数之内

--- C++study.cpp
+++ C++study.cpp
@@ -2,55 +2,39 @@
 #include<string>
 using namespace std;
 
-//结构体：属于用户自己定义的数据类型，用于用户存储不同的数据类型
 
+//结构体数组：将自定义的结构体放入数组中方便维护
+//语法 struct 结构体名 数组名[元素个数] = {{},{},{},...{}}；
 
-//1.创建学生数据类型：包括（姓名、年龄、分数）
-//自定义数据类型：一些类型集合组成的一个类型
-//语法 struct 类型名称｛成员列表｝
+//结构体除了定义结构体可以在main之外，定义和赋值需要在main函数之内做
 
-struct  student
+//1.定义结构体
+struct student
 {
-	//成员类别
-	//姓名
 	string name;
-	//年龄
 	int age;
-	//分数
 	int grade;
-}s3;//创建结构体的时候 顺便创建结构体变量
+};
+
 
 int main()
 {
-	//2.通过学生类型创建具体学生
-	//2.1 struct student s1;
-	student s1;//创建结构体变量的时候，关键字struct可以省略，但是创建的时候不可以省略
-	//给s1属性赋值，通过.访问结构体变量中的属性
-	s1.name = "张三";
-	s1.age = 18;
-	s1.grade = 95;
-	cout << "姓名：" << s1.name << endl;
-	cout << "年龄：" << s1.age << endl;
-	cout << "成绩：" << s1.grade << endl;
-
-	//2.2 struct student s2 = { , , };
-	struct student s2 = {
-		"李四",
-		19,
-		65
+	//2.创建结构体数组
+	student arr[2]
+	{
+		{"张三",18,65},
+		{"李四",17,95}
 	};
-	cout << "姓名：" << s2.name << endl;
-	cout << "年龄：" << s2.age << endl;
-	cout << "成绩：" << s2.grade << endl;
-
-	//2.3 在定义结构体时顺便创建结构体变量,一般不用该方式
-	s3.name = "王五";
-	s3.age = 20;
-	s3.grade = 96;
-	cout << "姓名：" << s3.name << endl;
-	cout << "年龄：" << s3.age << endl;
-	cout << "成绩：" << s3.grade << endl;
-
+	//3.给结构体数组中的元素赋值
+	arr[0].name = "王五";//把结构体数组中的第一个元素中的姓名更改
+	
+	//遍历结构体数组
+	for (int i = 0; i < 2; i++)
+	{
+		cout << arr[i].name << endl;
+		cout << arr[i].age << endl;
+		cout << arr[i].grade << endl;
+	}
 
 	system("pause");
 	return 0;
