22dec802febfc612a3c5319bafbdcb445de120cc
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-29 12:26:04 +0800

    常量引用防止误操作

--- C++study.cpp
+++ C++study.cpp
@@ -2,24 +2,23 @@
 #include<string>
 using namespace std;
 
-//引用的本质：在C++内部实现是一个指针常量
+//常量引用主要用来修饰形参，防止误操作
 
-void func(int& ref)//发现是引用，转换为int *const ref = &a
+//打印输出
+void fun(const int& val)
 {
-	ref = 100;//ref是引用，转换为*ref = 100;
+	//val = 1000;
+	cout << val << endl;
 }
 int main()
 {
-	int a = 10;
-	/*int& ref = a;//自动转化为int *const ref = &a,指针常量：指向不可以更改，也就是说明引用为什么不可以更改
-	ref = 20;//内部发现ref是引用，自动帮我们转换为：*ref = 20
+	//int a = 10;
+	//int& b = 10;//引用必须引用一块合法的内存空间
+	//const int & b = 10;//加上const,编译器将代码修改 int tremp = 10; const int & ref = 10
+	//ref = 20;//加入const之后 变为只读，不可修改
 
-	cout << "a=" << a << endl;
-	cout << "ref=" << ref << endl;//只要碰到引用，就会自动做解引用的操作*/
-	
-	func(a);
-	cout << "a=" << a << endl;
-	//cout << "ref=" << ref << endl;
+	int a = 10;
+	fun(a);
 	system("pause");
 	return 0;
 }
