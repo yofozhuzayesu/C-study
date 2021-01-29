2e8a33864c597c8ec81abe4b6cc79334cc270f48
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-01-29 11:37:10 +0800

    利用引用做函数参数
    可以修饰实参

--- C++study.cpp
+++ C++study.cpp
@@ -3,21 +3,46 @@
 using namespace std;
 
 
-//引用
-//作用：给变量起别名
-//语法：数据类型 &别名 = 原名
+//引用做函数参数
+//普通值传递时，实参无法修饰
+//利用引用的技术可以让形参修饰实参，简化指针操作
 
-//引用注意事项
-//引用必须初始化
-//引用在初始化后，不可以改变
+//值传递
+void swap01(int a, int b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+//地址传递
+void swap02(int *p1,int *p2)
+{
+	int temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
+}
+
+void swap03(int &a, int &b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
 int main()
 {
-	int a = 20;
-	int c = 10;
-	int &b = a;//引用初始化，不能写int &b;
-	//int &b = c;引用初始化之后，不可以改变
-	b = 20;
+	int a = 10;
+	int b = 20;
+	/*swap01(a, b);//值传递
 	cout << a << endl;
+	cout << b << endl;
+
+	swap02(&a, &b);//地址传递
+	cout << a << endl;
+	cout << b << endl;*/
+
+	swap03(a, b);//引用传递
+	cout << a << endl;//实参发生改变
 	cout << b << endl;
 	system("pause");
 	return 0;
