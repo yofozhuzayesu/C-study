90eb3178ef6094f00539450ec39aebc9e0dc61bd
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-02-18 14:48:02 +0800

    函数重载的条件

--- C++study.cpp
+++ C++study.cpp
@@ -2,22 +2,55 @@
 #include<string>
 using namespace std;
 
-//函数的占位参数
-//返回值类型 函数名 （数据类型）{}
+//函数重载：函数名可以相同，提高复用性
+//满足条件
+//1.同一个作用域下
+//2.函数名称相同
+//3.函数参数类型不同或者个数不同或者顺序不同
 
-//目前阶段的占位参数用不到，后面课程会用到
-//占位参数 还可以有默认参数
-void func(int a,int = 10)
+//注意：函数的返回值不可用作为函数重载的条件
+
+void func()
 {
-	cout << a << endl;
+	cout << "函数1的调用" << endl;
+
 }
 
+void func(int a)
+{
+	cout << "函数2的调用" << endl;
+
+}
+void func(double a)
+{
+	cout << "函数3的调用" << endl;
+
+}
+void func(double a,int b )
+{
+	cout << "函数4的调用" << endl;
+
+}
+
+void func(int a ,double b)
+{
+	cout << "函数5的调用" << endl;
+
+}
+//函数的返回值不可以作为重载的条件
+/*int func(int a, double b)
+{
+	cout << "函数5的调用" << endl;
+
+}*/
 int main()
 {
-	func(20);
-
+	//func();//调用函数1
+	//func(20);//调用函数2
+	//func(3.14);//调用函数3
+	//func(3.14, 10);//调用函数4
+	func(2, 3.14);//调用函数5
 	system("pause");
-
 	return 0;
 }
 
