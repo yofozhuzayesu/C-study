15b14c7f1fe02a72120f2ec6fe34c1e172b9e3f5
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-02-18 14:39:49 +0800

    函数的占位参数

--- C++study.cpp
+++ C++study.cpp
@@ -2,36 +2,22 @@
 #include<string>
 using namespace std;
 
+//函数的占位参数
+//返回值类型 函数名 （数据类型）{}
 
-//函数默认参数
-//在C++中，函数的形参列表中的形参是可以有默认值的
-//语法：返回值类型 函数名 （参数=默认值）｛｝
-
-int func(int a = 10, int b = 20)
+//目前阶段的占位参数用不到，后面课程会用到
+//占位参数 还可以有默认参数
+void func(int a,int = 10)
 {
-	return a + b;
-
+	cout << a << endl;
 }
-//注意事项
-
-//1.如果某个位置已经有了默认参数，那么从这个位置往后从左到右都必须有默认值
-//int func2(int a, int c = 10, int b)
-//{
-//}
-
-//2.如果函数的声明有了默认参数，函数的实现（定义）就不能由默认参数
-//int func3(int a=10, int b=10);
-//int func3(int a = 10, int b = 10)
-//{
-//	return a + b;
-//}
-
 
 int main()
 {
-	int a = func(20,30);//如果有数据传入就用传入的数据，如果没有数据传入，则用默认的数据
-	cout << "a=" << a << endl;
+	func(20);
+
 	system("pause");
+
 	return 0;
 }
 
