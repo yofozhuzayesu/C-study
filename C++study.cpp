4695e7c2e13588cc136b6dfb178f1dea4819af5c
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-02-18 16:53:36 +0800

    类和对象-成员属性设置为私有

--- C++study.cpp
+++ C++study.cpp
@@ -2,28 +2,49 @@
 #include<string>
 using namespace std;
 
-//struct 和 class 的区别：默认的访问权限不同
-/*
-1.struct默认权限为公共
-2.class默认权限为私有
-*/
+//成员属性设置为私有
+//1.可以自己控制读写权限
+//2.对于写可以检测数据的有效性
 
-class C1
+class person
 {
-	int m_a;//不标注是public还是private 默认权限是私有
+public:
+	//设置姓名
+	void setname(string p_name)
+	{
+		name = p_name;
+	}
+	//获取姓名
+	string getname()
+	{
+		return name;
+	}
+	//获取年龄 只读
+	int getage()
+	{
+		age = 18;
+		return age;
+	}
+	//设置爱人 只写
+	void setlover(string p_lover)
+	{
+		lover = p_lover;
+	}
+private:
+	string name;//可读可写
+	int age;//只读
+	string lover;//只写
 
 };
 
-struct C2
-{
-	int m_b;//默认权限是公共权限
-};
+
 int main()
 {
-	C1 c1;
-	//c1.m_a = 100; 私有权限无法访问 
-	C2 c2;
-	c2.m_b = 100;//公共权限可以访问 
+	person p1;
+	p1.setname("张三");
+	cout << "姓名为：" << p1.getname() << endl;//属性设置为私有，行为设置为公共，通过行为获取属性
+	cout << "年龄为：" << p1.getage() << endl;
+	p1.setlover("李四");
 	system("pause");
 	return 0;
 }
