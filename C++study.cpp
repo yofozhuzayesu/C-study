f7dda87a87b3727e18ceeb83133d51cc04136209
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-02-18 15:01:01 +0800

    函数重载注意事项

--- C++study.cpp
+++ C++study.cpp
@@ -2,54 +2,36 @@
 #include<string>
 using namespace std;
 
-//函数重载：函数名可以相同，提高复用性
-//满足条件
-//1.同一个作用域下
-//2.函数名称相同
-//3.函数参数类型不同或者个数不同或者顺序不同
-
-//注意：函数的返回值不可用作为函数重载的条件
-
-void func()
+//函数重载的注意事项
+//1.引用作为重载的条件
+void func(int &a)
 {
-	cout << "函数1的调用" << endl;
+	cout << "func(int &a)的调用" << endl;
 
 }
-
-void func(int a)
+void func(const int &a)
 {
-	cout << "函数2的调用" << endl;
+	cout << "func(const int &a)的调用" << endl;
 
 }
-void func(double a)
+//2.函数重载碰到默认参数
+void func2(int a,int b = 10)
 {
-	cout << "函数3的调用" << endl;
-
+	cout << "func2(int a,int b = 10 )的调用" << endl;
 }
-void func(double a,int b )
+void func2(int a)
 {
-	cout << "函数4的调用" << endl;
-
+	cout << "func2(int a )的调用" << endl;
 }
 
-void func(int a ,double b)
-{
-	cout << "函数5的调用" << endl;
-
-}
-//函数的返回值不可以作为重载的条件
-/*int func(int a, double b)
-{
-	cout << "函数5的调用" << endl;
-
-}*/
 int main()
 {
-	//func();//调用函数1
-	//func(20);//调用函数2
-	//func(3.14);//调用函数3
-	//func(3.14, 10);//调用函数4
-	func(2, 3.14);//调用函数5
+	//int a = 10;
+	//func(a);//调用函数1，原因是a是变量是可读可写状态，所以调用的是第一个函数
+	//func(10);//10的传递方式：相当于const int &a = 10,编译器自动分配一个临时空间存放a，用1的话是int &a = 10不合法
+
+	//func2(10);//当函数重载碰到默认参数，会出现二义性报错，应该尽量避免这种情况
+
 	system("pause");
 	return 0;
 }
