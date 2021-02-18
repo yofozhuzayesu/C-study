e2699ea099e0d1a72b8f0a6a1e730a5d5b0adc2a
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-02-18 19:10:49 +0800

    类和对象案例2-判断点和圆的关系

--- C++study.cpp
+++ C++study.cpp
@@ -2,109 +2,79 @@
 #include<string>
 using namespace std;
 
-//设计一个立方体类
-//1.创建立方体类
-//2.设计属性
-//3.设计行为 获取立方体面积和体积
-//4.分别利用全局函数和成员函数判断两个立方体是否相等
-class cube
+//设计一个圆形类，一个点类，计算点和圆的关系
+class cirlce
 {
 public:
-	//设置长
-	void setL(int L)
+	void setXYR(int a,int b,int c)//设置圆心的坐标和半径
 	{
-		m_L = L;
+		x = a;
+		y = b;
+		r = c;
 	}
-	//设置宽
-	void setW(int W)
+	int getX()
 	{
-		m_W = W;
+		return x;
 	}
-	//设置高
-	void setH(int H)
+	int getY()
 	{
-		m_H = H;
+		return y;
 	}
-	//获取长
-	int getL()
+	int getR()
 	{
-		return m_L;
-	}
-	//获取宽
-	int getW()
-	{
-		return m_W;
-	}
-	//获取高
-	int getH()
-	{
-		return m_H;
-	}
-	//计算体积
-	int cubeV()
-	{
-		return m_L * m_W * m_H;
-	}
-	//计算面积
-	int cubeS()
-	{
-		return (m_L*m_H + m_L * m_W + m_W * m_H) * 2;
-	}
-
-	//利用成员函数判断两个立方体是否相同
-	bool isSameByClass(cube &c)
-	{
-		if (m_L==c.getL()&&m_H==c.getH()&&m_W==c.getW())
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		return r;
 	}
 private:
-	int m_L;
-	int m_W;
-	int m_H;
-
+	int x;//横坐标
+	int y;//纵坐标	
+	int r;//半径
+};
+class point
+{
+public:
+	void setXY(int a, int b)//设置点的坐标
+	{
+		x = a;
+		y = b;
+	}
+	int getX()
+	{
+		return x;
+	}
+	int getY()
+	{
+		return y;
+	}
+private:
+	int x;
+	int y;
 };
 
-//利用全局函数来判断立方体是否相同
-bool isSame(cube &c1, cube &c2)//利用引用的方式传递数据，防止再拷贝一份数据
+int relation(cirlce &c, point &p)
 {
-	if (c1.getL()==c2.getL()&&c1.getH()==c2.getH()&&c1.getW()==c2.getW())
-	{
-		return true;
-	}
-	return false;
+	int d = (c.getX() - p.getX())*(c.getX() - p.getX()) + (c.getY() - p.getY())*(c.getY() - p.getY());
+	return d;
 }
-
 int main()
 {
-	cube c1;//创建第一个立方体
-	c1.setH(10);
-	c1.setL(10);
-	c1.setW(10);
-	cout << "立方体的体积为：" << c1.cubeV() << endl;
-	cout << "立方体面积为：" << c1.cubeS() << endl;
-
-	cube c2;//创建第二个立方体
-	c2.setH(10);
-	c2.setL(10);
-	c2.setW(10);
-
-	//bool ret = isSame(c1, c2);//利用全局函数判断
-	bool ret = c1.isSameByClass(c2);//利用成员函数判断是否相同
-	if (ret)
+	cirlce c1;
+	point p1;
+	c1.setXYR(0, 0, 5);
+	p1.setXY(3,4);
+	int d = relation(c1, p1);
+	if (d>(c1.getR()*c1.getR()))
 	{
-		cout << "这两个立方体相同" << endl;
-
+		cout << "点在圆外" << endl;
+	}
+	else if (d == (c1.getR()*c1.getR()))
+	{
+		cout << "点在圆上" << endl;
 	}
 	else
 	{
-		cout << "这两个立方体不同" << endl;
+		cout << "点在圆内" << endl;
 	}
+
 	system("pause");
 	return 0;
 }
