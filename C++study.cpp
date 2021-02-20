b037045eb2d923be8fe8ccc86a0899bddc4f0db4
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-02-20 23:40:47 +0800

    类和对象-拷贝构造函数的调用规则

--- C++study.cpp
+++ C++study.cpp
@@ -2,24 +2,29 @@
 #include<string>
 using namespace std;
 
-//拷贝构造函数的调用时机
-//1.使用一个已经创建完毕的对象来初始化一个新对象
-//2.值传递的方式给函数参数传值
-//3.以值方式返回局部对象
+//构造函数调用规则
+//默认情况下，C++编译器至少给一个类添加三个函数
+//1.默认构造函数（无参，函数体为空）
+//2.默认析构函数（无参，函数体为空）
+//3.默认拷贝构造函数（对属性进行值拷贝）
+
+//构造函数的调用规则
+//1.如果用户定义有参构造函数，C++就不在提供无参构造，但是会默认提供拷贝构造
+//2.如果用户定义拷贝构造函数，C++不会再提供其他构造函数
 
 class person
 {
 public:
-	person()
+	/*person()
 	{
 		cout << "person默认构造函数调用" << endl;
-	}
+	}*/
 	person(int age)
 	{
 		p_age = age;
 		cout << "person有参构造函数调用" << endl;
 	}
-	person(const person &p)
+	person(const person &p)    //在不定义拷贝构造函数的情况下，编译器默认提供拷贝构造函数
 	{
 		p_age = p.p_age;
 		cout << "person拷贝构造函数调用" << endl;
@@ -32,40 +37,23 @@ public:
 	int p_age;
 };
 
-//1.使用一个已经创建完毕的对象来初始化一个新对象
-void test01()//使用一个已经创建完毕的对象来初始化一个新对象
-{
-	person p1(20);
-	person p2(p1);
-	cout << "p2的年龄是：" << p2.p_age << endl;
-}
+//void test01()
+//{
+//	person p;
+//	p.p_age = 18;
+//	person p2(p);
+//	cout << p2.p_age << endl;
+//}
 
-//2.值传递的方式给函数参数传值
-void dowork(person p)
-{
-	
-}
 void test02()
 {
-	person p;
-	dowork(p);//值传递,会调用拷贝构造函数（值传递本身就是创建一个临时副本）
-}
-
-//3.以值方式返回局部对象
-person dowork2()
-{
-	person p1;//创建的是局部变量，函数调用结束后会释放
-	return p1;//所以这里返回的是创建的一个新的对象（拷贝复制而来）
-}
-void test03()
-{
-	person p = dowork2();
+	person p(18);
+	person p2(p);
 }
 int main()
 {
 	//test01();
-	//test02();
-	//test03();
+	test02();
 	system("pause");
 	return 0;
 }
