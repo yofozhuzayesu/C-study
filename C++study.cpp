1c4a7c91762c5538e36b4f9e7ad3c8332686f858
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-02-21 10:51:51 +0800

    类和对象-初始化列表
    注意初始化列表的语法，其中的冒号写在哪

--- C++study.cpp
+++ C++study.cpp
@@ -2,60 +2,40 @@
 #include<string>
 using namespace std;
 
-//深拷贝与浅拷贝
-//浅拷贝：简单的赋值拷贝操作
-//深拷贝：在堆区重新申请空间，进行拷贝操作
+//初始化列表
+//作用：C++提供了初始化列表语法，用来初始化属性
+//语法：构造函数()：属性1（值1），属性2（值2）...｛｝
 
 class person
 {
+	
 public:
-	person()
-	{
-		cout << "person默认构造函数调用" << endl;
-	}
-	person(int age,int height)
-	{
-		p_age = age;
-		p_Height=new int(height);
-		cout << "person有参构造函数调用" << endl;
-	}
-	person(const person &p)    //在不定义拷贝构造函数的情况下，编译器默认提供拷贝构造函数
-	{
-		p_age = p.p_age;
-		//p_Height = p.p_Height 编译器默认实现就是这行代码——浅拷贝
-		//深拷贝
-		p_Height = new int(*p.p_Height);
-		cout << "person拷贝构造函数调用" << endl;
-	}
-	~person()
-	{
-		//析构代码作用，将堆区开辟数据做释放操作
-		if (p_Height != NULL)//如果不为空就置为空
-		{
-			delete p_Height;
-			p_Height = NULL;//规范操作
-		}
-		//浅拷贝带来的问题就是堆区的内存重复释放
-		//根据先进后出的顺序，拷贝出来的对象P2会先释放堆区的数据，导致P1再释放堆区的数据的时候出现错误
-		//解决方法：浅拷贝的问题要利用深拷贝解决，就是自己再写一个拷贝函数，重新开辟一个堆区存放数据
+	////传统初始化操作
+	//person(int a,int b,int c)
+	//{
+	//	p_a = a;
+	//	p_b = b;
+	//	p_c = c;
+	//}
 
-
-		cout << "person默认析构函数调用" << endl;
+	//初始化列表初始化属性
+	person(int a,int b,int c) :p_a(a), p_b(b), p_c(c)//利用该方式使得初始化更灵活
+	{
 
 	}
-	int p_age;
-	int *p_Height;//身高
+	//属性
+	int p_a;
+	int p_b;
+	int p_c;
+
 };
-
 void test01()
 {
-	person p1(18,180);
-	cout << "p1的年龄：" << p1.p_age << endl;
-	cout << "p1的身高：" << *p1.p_Height << endl;
-
-	person p2(p1);
-	cout << "p2的年龄：" << p2.p_age << endl;//年龄是浅拷贝
-	cout << "p2的身高：" << *p2.p_Height << endl;//身高是深拷贝
+	//person p(10, 20, 30);//传统方式初始化（有参构造函数不使用初始化列表需要进行赋值操作）
+	person p(10,20,30);//初始化列表
+	cout << p.p_a << endl;
+	cout << p.p_b << endl;
+	cout << p.p_c << endl;
 }
 int main()
 {
