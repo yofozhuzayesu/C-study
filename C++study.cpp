d00a9c7a0a50afb78317a9be0915ad63a9a3e580
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-02-21 11:16:29 +0800

    类与对象-类对象作为类的成员
    注意类对象作为成员时候的隐式转换法，调用构造函数和析构函数的顺序

--- C++study.cpp
+++ C++study.cpp
@@ -2,41 +2,53 @@
 #include<string>
 using namespace std;
 
-//初始化列表
-//作用：C++提供了初始化列表语法，用来初始化属性
-//语法：构造函数()：属性1（值1），属性2（值2）...｛｝
+//类对象作为类成员
+//C++类中的成员可以是另一个类的对象，我们称该成员为对象成员
+//思考：类对象作为另一个类的成员的时候，两个类的构造函数的调用顺序
+
+
+class phone
+{
+public:
+	string PName;//手机品牌
+	phone(string pname)
+	{
+		cout << "调用了phone类的有参构造函数" << endl;
+		PName = pname;
+	}
+
+	~phone()
+	{
+		cout << "调用了phone类的析构函数" << endl;
+	}
+};
 
 class person
 {
-	
 public:
-	////传统初始化操作
-	//person(int a,int b,int c)
-	//{
-	//	p_a = a;
-	//	p_b = b;
-	//	p_c = c;
-	//}
+	string p_name;//姓名
+	phone p_phonename;//手机
 
-	//初始化列表初始化属性
-	person(int a,int b,int c) :p_a(a), p_b(b), p_c(c)//利用该方式使得初始化更灵活
+	//p_phonename(phonename) 相当于phone PName = phonename  隐式转换法
+	person(string name, string phonename):p_name(name),p_phonename(phonename)//初始化列表方式初始化
 	{
-
+		cout << "调用person类的有参构造函数" << endl;
 	}
-	//属性
-	int p_a;
-	int p_b;
-	int p_c;
 
+	~person()
+	{
+		cout << "调用person类的析构函数" << endl;
+	}
 };
+
+//当其他类的对象作为本类的成员，构造的时候先构造类对象，在构造自身
+//析构的顺序与构造相反，先析构类对象，再析构类内部的对象
 void test01()
 {
-	//person p(10, 20, 30);//传统方式初始化（有参构造函数不使用初始化列表需要进行赋值操作）
-	person p(10,20,30);//初始化列表
-	cout << p.p_a << endl;
-	cout << p.p_b << endl;
-	cout << p.p_c << endl;
+	person p("张三", "华为");
+	cout << p.p_phonename.PName << endl;//输出语句要注意，格式是A类.B类.B类中的属性
 }
+	
 int main()
 {
 	test01();
