08b4115e7e3568859f87e105fd8217fb78f45ea7
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-02-23 11:44:37 +0800

    类和对象-空指针也可以访问成员函数
    但是空指针访问成员函数的时候，要避免出现空指针访问成员属性的坑，解决方法是添加一个if判断

--- C++study.cpp
+++ C++study.cpp
@@ -2,57 +2,43 @@
 #include<string>
 using namespace std;
 
-//成员变量和成员函数是分开储存的
-//每一个非静态成员函数只会诞生一份函数实例，也就是说多个同类型的对象会公用一块代码
-//问题：这一块代码市如何区分那个对象调用自己的呢
+//空指针访问成员函数
+//C++中空指针也是可以调用成员函数的，但是也要注意有没有用到this指针
 
-//C++通过提供特殊的对象指针，this指针，解决上述问题，this指针指向被调用的成员函数所属的对象
-
-//this指针是隐含每一个非静态成员函数内的一种指针
-//this指针不需要定义，直接使用即可
-
-//this指针的用途：
-//当形参和成员变量同名时，可用this指针来区分
-//在类的非静态成员函数中返回对象本身，可用return *this
+//如果用到this指针，需要加以判断保证代码的健壮性
 
 class person
 {
 public:
-	person(int age)
+	void showpersonname()
 	{
-		//age = age; //同名冲突
-		//this指针指向的是被调用的成员函数所属的对象
-		this->age = age;
+		cout << "this is person class" << endl;
 	}
-	person& personAddage(person &p)//返回本体对象，需要使用引用的方式返回
+	void showpersonage()
 	{
-		this->age += p.age;//相当于把形参对象中的年龄+到被调用的成员函数所的对象上
-		//this指向p2的指针，而*this指向的就是p2这个对象本体
-		return *this;
+		//报错原因：传入的指针是NULL指针（也就是this也是指向的空），无法访问其中的属性
+		//解决方法：添加一个判断
+		if (this ==NULL)
+		{
+			return;//为空就直接退出，否则就执行下一行
+		}
+		cout << "person age = " << this->m_age << endl;//报错
 	}
-	int age;
+	int m_age;
 
 };
 
-//解决名称冲突
 void test01()
 {
-	person p1(18);
-	cout << p1.age << endl;
+	person *p = NULL;
+	p->showpersonname();
+	p->showpersonage();
 }
 
-void test02()
-{
-	person p1(10);
-	person p2(10);
-	//链式编程思想
-	p2.personAddage(p1).personAddage(p2).personAddage(p2);//函数返回的是一个对象，可以继续再调用对象的成员函数
-	cout << "p2的年龄：" << p2.age << endl;
-}
+
 int main()
 {
-	test01();//成员属性和形参同名，会出现问题，解决方法：1.规范编码 2.使用this
-	test02();
+	test01();
 	system("pause");
 	return 0;
 }
