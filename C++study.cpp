ff17c6c012f3d6c93d1c0029a789b3f25090b8e9
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-02-23 09:36:10 +0800

    类和对象-静态成员

--- C++study.cpp
+++ C++study.cpp
@@ -2,53 +2,51 @@
 #include<string>
 using namespace std;
 
-//类对象作为类成员
-//C++类中的成员可以是另一个类的对象，我们称该成员为对象成员
-//思考：类对象作为另一个类的成员的时候，两个类的构造函数的调用顺序
-
-
-class phone
-{
-public:
-	string PName;//手机品牌
-	phone(string pname)
-	{
-		cout << "调用了phone类的有参构造函数" << endl;
-		PName = pname;
-	}
-
-	~phone()
-	{
-		cout << "调用了phone类的析构函数" << endl;
-	}
-};
+//静态成员
+//静态成员就是在成员变量和成员函数前加上关键字static，称为静态成员
+//分为：
+//静态成员变量：所有对象共享同一份数据
+//			    在编译阶段分配内存
+//			    类内声明，类外初始化
+//静态成员函数：所有对象共享一个函数
+//			    静态成员函数只能访问静态成员变量
 
 class person
 {
 public:
-	string p_name;//姓名
-	phone p_phonename;//手机
-
-	//p_phonename(phonename) 相当于phone PName = phonename  隐式转换法
-	person(string name, string phonename):p_name(name),p_phonename(phonename)//初始化列表方式初始化
+	//静态成员函数
+	static void func()
 	{
-		cout << "调用person类的有参构造函数" << endl;
+		m_a = 100;//静态成员函数可以访问静态成员变量
+		//m_b = 200;  //会出错，静态成员函数 不可以访问非静态成员变量,因为无法区分到底是哪个对象的属性
+		cout << "static静态成员函数的调用" << endl;
 	}
+	static int m_a;//静态成员变量 需要在类内声明，类外初始化
+	int m_b;//非静态成员变量
 
-	~person()
+
+	//静态成员函数也是有访问权限的
+private:
+	static void func2()
 	{
-		cout << "调用person类的析构函数" << endl;
+		cout << "static void func2函数的调用" << endl;
 	}
 };
 
-//当其他类的对象作为本类的成员，构造的时候先构造类对象，在构造自身
-//析构的顺序与构造相反，先析构类对象，再析构类内部的对象
+int person::m_a = 0;//静态成员变量在类外初始化
+
+
+//静态成员函数的两种访问方式
 void test01()
 {
-	person p("张三", "华为");
-	cout << p.p_phonename.PName << endl;//输出语句要注意，格式是A类.B类.B类中的属性
+	//通过对象调用
+	person p;
+	p.func();
+
+	//通过类名访问
+	person::func();
+	//person::func2();私有静态成员函数在类外不可访问
 }
-	
 int main()
 {
 	test01();
