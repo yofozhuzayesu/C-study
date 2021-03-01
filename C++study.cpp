f6646d7ea37c851df5f691f50480be72c48af7e7
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-01 17:56:50 +0800

    类和对象-运算符重载+

--- C++study.cpp
+++ C++study.cpp
@@ -2,59 +2,57 @@
 #include<string>
 using namespace std;
 
-//在程序里，有些私有属性也想让类外特殊的一些函数或者类进行访问，就需要用到友元的技术
-//友元的目的就是让一个函数或者类访问另一类中的私有成员
-//友元的关键字是 frined
+//运算符重载：对已有的运算符重新进行定义，赋予其另一种功能，以适应不同的数据类型
 
-//友元三种实现
-//1.全局函数做友元
-//2.类做友元
-//3.成员函数做友元
+//加号运算符重载：实现两个自定义数据类型相加的运算
 
-//成员函数做友元
-class Building;//如果类B使用类A，要么按顺序，要么先声明
-class GoodGay
+
+
+class person
 {
 public:
-	GoodGay();
-	void visit();//让visit函数可以访问私有属性
-	void visit2();//visit2函数不可以访问私有属性
-	Building *b;
+	int m_a;
+	int m_b;
+	//1.成员函数重载+号
+	/*person operator +(person &p)
+	{
+		person temp;
+		temp.m_a = this->m_a + p.m_a;
+		temp.m_b = this->m_b + p.m_b;
+		return temp;
+	}*/
 };
-class Building
+
+//通过全局函数重载+号
+person operator+(person &p1, person &p2)
 {
-	//表示GoodGay类下的visit成员函数作为本类的友元
-	friend void GoodGay::visit();
-public:
-	Building();
-	string m_sittingroom;//客厅	
-private:
-	string m_bedroom;//卧室
-};
-GoodGay::GoodGay()
-{
-	b = new Building;//创建Building类型的数据，需要放在Building类定义之后，不然编译器不清楚创建的类的内存大小
+	person temp;
+	temp.m_a = p1.m_a + p2.m_a;
+	temp.m_b = p1.m_b + p2.m_b;
+	return temp;
 }
-Building::Building()
-{
-	m_sittingroom = "客厅";
-	m_bedroom = "卧室";
-}
-void GoodGay::visit()
-{
-	cout << "好基友类中的visit函数正在访问" << b->m_sittingroom << endl;
-	cout << "好基友类中的visit函数正在访问" << b->m_bedroom << endl;
-}
-void GoodGay::visit2()
-{
-	cout << "好基友类中的visit函数正在访问" << b->m_sittingroom << endl;
-}
+
 void test01()
 {
-	GoodGay g;
-	g.visit();
-	g.visit2();
+	person p1;
+	p1.m_a = 10;
+	p1.m_b = 20;
+	person p2;
+	p2.m_a = 10;
+	p2.m_b = 30;
+
+	person p3 = p1 + p2;
+	cout << p3.m_a << endl;
+	cout << p3.m_b << endl;
 }
+//成员函数重载本质调用
+//person p3 = p1.operator+(p2)
+
+//全局函数重载本质调用
+//person p3 = operator+(p1,p2)
+
+//person p3 = p1 + p2   
+//运算符重载，也可以发生函数重载，就是同样的名字 operator+ 可以定义不同的数据类型
 int main()
 {
 	test01();
