10b458c47537ac5d139c1a871069332ce35103b2
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-01 18:17:16 +0800

    类与对象-运算符重载<<
    注意cout是个ostream类

--- C++study.cpp
+++ C++study.cpp
@@ -2,57 +2,43 @@
 #include<string>
 using namespace std;
 
-//运算符重载：对已有的运算符重新进行定义，赋予其另一种功能，以适应不同的数据类型
-
-//加号运算符重载：实现两个自定义数据类型相加的运算
-
-
+//左移运算符重载 <<
 
 class person
 {
 public:
 	int m_a;
 	int m_b;
-	//1.成员函数重载+号
-	/*person operator +(person &p)
+	//利用成员函数重载左移运算符 p.operator<<(cout)  简化版本 p<<cout
+	//通常不会成员函数重载左移运算符  因为无法实现cout在左侧
+	/*void operator<<(cout)
 	{
-		person temp;
-		temp.m_a = this->m_a + p.m_a;
-		temp.m_b = this->m_b + p.m_b;
-		return temp;
+		
+		
 	}*/
 };
 
-//通过全局函数重载+号
-person operator+(person &p1, person &p2)
+//只能利用全局函数重载左移运算符
+//cout属于输出流对象
+ostream & operator<<(ostream &cout,person p) //本质是operator<<(cout,p)  简化成cout<<p
 {
-	person temp;
-	temp.m_a = p1.m_a + p2.m_a;
-	temp.m_b = p1.m_b + p2.m_b;
-	return temp;
+	cout << p.m_a << endl;
+	cout << p.m_b << endl;
+	return cout;
 }
-
 void test01()
 {
-	person p1;
-	p1.m_a = 10;
-	p1.m_b = 20;
-	person p2;
-	p2.m_a = 10;
-	p2.m_b = 30;
-
-	person p3 = p1 + p2;
-	cout << p3.m_a << endl;
-	cout << p3.m_b << endl;
+	person p;
+	p.m_a = 10;
+	p.m_b = 20;
+	//cout << p << endl;
+	//链式编程思想
+	//正常情况下，cout有返回值，可以继续追加对象，但是目前定义的是无返回值
+	//修改方式是将重载函数改成有返回值的，返回的对象类型为ostream
+	cout << p << "Hello world" << endl;
 }
-//成员函数重载本质调用
-//person p3 = p1.operator+(p2)
 
-//全局函数重载本质调用
-//person p3 = operator+(p1,p2)
 
-//person p3 = p1 + p2   
-//运算符重载，也可以发生函数重载，就是同样的名字 operator+ 可以定义不同的数据类型
 int main()
 {
 	test01();
