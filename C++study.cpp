f47d60a33b68e11dae5f76ff40bdf59aa5689f1a
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-02 23:12:25 +0800

    类和对象-继承-基本语法
    继承的好处：减少重复的代码

--- C++study.cpp
+++ C++study.cpp
@@ -2,53 +2,162 @@
 #include<string>
 using namespace std;
 
-//函数调用符() 也可以重载
-//由于重载后使用的方式非常像函数的调用，因此称为仿函数
-//仿函数没有固定写法，非常灵活
+//继承——面向对象的三大特性之一
+//例如类与类之间存在特殊的关系，比如动物中有猫科动物，猫科动物又有老虎豹子等
+//定义这些类的时候，下级别的成员除了拥有上一级的共性，还有自己的特性
+//利用继承的技术就能减少重复代码
 
-class MYprint
+//example  很多网站有公共的头部、底部甚至公共得到左侧列表，只有中心内容不同
+//利用普通写法和继承的写法来实现网页中的内容，看看继承存在的意义以及好处
+
+//普通实现
+//JAVA页面
+//class JAVA
+//{
+//public:
+//	void header()
+//	{
+//		cout << "首页、公开课、登录、注册（公共头部）" << endl;
+//	}
+//	void footer()
+//	{
+//		cout << "帮助中心、交流合作、站内地图（公共底部）" << endl;
+//	}
+//	void left()
+//	{
+//		cout << "JAVA PYTHON C++（公共分类列表）" << endl;
+//	}
+//	void content()
+//	{
+//		cout << "JAVA学科视频" << endl;
+//	}
+//};
+//
+//class PYTHON
+//{
+//public:
+//	void header()
+//	{
+//		cout << "首页、公开课、登录、注册（公共头部）" << endl;
+//	}
+//	void footer()
+//	{
+//		cout << "帮助中心、交流合作、站内地图（公共底部）" << endl;
+//	}
+//	void left()
+//	{
+//		cout << "JAVA PYTHON C++（公共分类列表）" << endl;
+//	}
+//	void content()
+//	{
+//		cout << "PYTHON学科视频" << endl;
+//	}
+//};
+//
+//
+//class cpp
+//{
+//public:
+//	void header()
+//	{
+//		cout << "首页、公开课、登录、注册（公共头部）" << endl;
+//	}
+//	void footer()
+//	{
+//		cout << "帮助中心、交流合作、站内地图（公共底部）" << endl;
+//	}
+//	void left()
+//	{
+//		cout << "JAVA PYTHON C++（公共分类列表）" << endl;
+//	}
+//	void content()
+//	{
+//		cout << "C++学科视频" << endl;
+//	}
+//};
+//
+//
+
+
+//继承写法
+//好处：减少重复的代码
+//语法：class 子类 : 继承方式 父类
+//子类也称为派生类 父类也称为基类
+//公共元素
+class BasePage
 {
 public:
-	//重载的函数调用运算符
-	void operator()(string text)
+	void header()
 	{
-		cout << text << endl;
+		cout << "首页、公开课、登录、注册（公共头部）" << endl;
+	}
+	void footer()
+	{
+		cout << "帮助中心、交流合作、站内地图（公共底部）" << endl;
+	}
+	void left()
+	{
+		cout << "JAVA PYTHON C++（公共分类列表）" << endl;
+	}
+};
+//JAVA类 继承Base
+class JAVA :public BasePage
+{
+public:
+	void content()
+	{
+		cout << "JAVA学科视频" << endl;
+	}
+};
+//C++类
+class cpp :public BasePage
+{
+public:
+	void content()
+	{
+		cout << "C++学科视频" << endl;
+	}
+};
+//PYTHON类 
+class PYTHON :public BasePage
+{
+public:
+	void content()
+	{
+		cout << "PYTHON学科视频" << endl;
 	}
 };
 
 void test01()
 {
-	MYprint p;
-	p("hello world");//由于使用起来非常类似函数调用，因此称为仿函数
+	cout << "JAVA下载页面如下" << endl;
+	JAVA j;
+	j.header();
+	j.left();
+	j.footer();
+	j.content();
+	cout << "---------------------------" << endl;
+
+	cout << "PYTHON下载页面如下" << endl;
+	PYTHON p;
+	p.header();
+	p.left();
+	p.footer();
+	p.content();
+	cout << "---------------------------" << endl;
+
+	cout << "C++下载页面如下" << endl;
+	cpp c;
+	c.header();
+	c.left();
+	c.footer();
+	c.content();
+	cout << "---------------------------" << endl;
 }
-
-//仿函数非常灵活，没有一个固定的写法
-//加法类
-class MYadd
-{
-public:
-	int operator()(int num1, int num2)
-	{
-		return num1 + num2;
-	}
-};
-
-void test02()
-{
-	MYadd a;
-	int sum = a(10, 20);
-	cout << sum << endl;
-
-	//匿名函数对象 就是未命名的对象使用函数重载符，因为局部对象函数结束完了就释放了，无需在保留
-	cout << MYadd()(100, 200) << endl;
-}
-
-
 
 int main()
 {
-	//test01();
-	test02();
+	test01();
 	system("pause");
 	return 0;
 }
