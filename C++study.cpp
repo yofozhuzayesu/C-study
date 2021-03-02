620982c2ac42af5c1c0cd9bf33528053a44a20f4
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-02 18:27:35 +0800

    类和对象-关系运算符重载

--- C++study.cpp
+++ C++study.cpp
@@ -2,67 +2,39 @@
 #include<string>
 using namespace std;
 
-//赋值运算符重载
-//C++编译器至少给一个类添加4个函数
-//1.默认构造函数（无参，函数体为空）
-//2.默认析构函数（无惨，函数体为空）
-//3.默认拷贝构造函数，对属性进行值拷贝
-//4.赋值运算符operator=，对属性进行值拷贝
-
-//如果类中有属性指向堆区，做赋值操作也会出现深浅拷贝问题
-
+//关系运算符重载，可以让两个自定义类型的对象进行对比操作
 class person
 {
 public:
-	person(int age)
+	person(string name, int age)
 	{
-		m_age = new int(age);//在堆区开辟一个数据，并返回该数据的指针
+		m_name = name;
+		m_age = age;
 	}
-	int *m_age;
-
-	~person()//堆区的数据要手动释放 在析构函数中释放
-			   //若不重载=号，P2=P1后，两个对象的*m_age指针都指向堆区同一块数据，会导致重复释放引发出错
-			   //解决方案，利用深拷贝解决浅拷贝带来的问题
+	bool operator==(person &p)
 	{
-		if (m_age !=NULL)
+		if (this->m_name ==p.m_name&&this->m_age==p.m_age)
 		{
-			delete m_age;
-			m_age = NULL;
+			return true;
 		}
+		return false;
 	}
-
-
-	//重载赋值运算符
-	person & operator=(person &p)//形参必须得用引用的方式，不然该函数执行完毕后，实参会释放，导致后面二次释放出错
-								 //返回值类型也需要用引用的方式，不用引用的话直接返回值，会复制一个新的对象
-	{
-		//编译器是提供浅拷贝
-		//m_age = p.m_age;
-
-		//应该先判断是否有属性在堆区，如果有则先释放，再深拷贝
-		if (this->m_age!=NULL)
-		{
-			delete this->m_age;
-			this->m_age = NULL;
-		}
-		//深拷贝操作
-		this->m_age = new int(*p.m_age);
-		//返回对象自身
-		return *this;
-	}
-
+	string m_name;
+	int m_age;
 };
-
 
 void test01()
 {
-	person p1(18);
-	person p2(20);
-	person p3(30);
-	p3 = p2 = p1;
-	cout << *p1.m_age << endl;
-	cout << *p2.m_age << endl;
-	cout << *p2.m_age << endl;
+	person p1("李四",18);
+	person p2("张三",18);
+	if (p1 == p2 )
+	{
+		cout << "p1和p2相等" << endl;
+	}
+	else
+	{
+		cout << "p1和p2不相等" << endl;
+	}
 }
 int main()
 {
