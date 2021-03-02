57fd864e24a051394442db52fbf218a366660ceb
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-02 23:58:10 +0800

    类和对象-继承了哪些成员属性
    父类中的所有非静态成员属性都会被子类继承下去，父类中的私有成员属性 是被编译器给隐藏了，因此是访问不到，但是确实被继承下去了

--- C++study.cpp
+++ C++study.cpp
@@ -2,102 +2,41 @@
 #include<string>
 using namespace std;
 
-//继承方式
-//继承方式：
-//        公共继承：能分别继承public和protected           无法访问父类private
-//        保护继承：public和protected全部继承为 protected 无法访问父类private
-//        私有继承：public和protected全部继承为 private   无法访问父类private
+//继承中的对象模型
+//问题：从父类继承过来的成员，哪些属于子类对象中
+
+class Base
+{
+public:
+	int m_a;
+protected:
+	int m_b;
+private:
+	int m_c;
+};
 
 //公共继承
-class base1
+class son1 :public Base
 {
 public:
-	int m_a;
-protected:
-	int m_b;
-private:
-	int m_c;
+	int m_d;
+
 };
 
-class son1 :public base1
-{
-public:
-	void func()
-	{
-		m_a = 10;//父类中的公共权限成员 到子类中依然是公共权限
-		m_b = 10;//父类中的保护权限成员 到子类中依然是保护权限
-		//m_c = 10;// 父类的私有权限成员 子类无法访问
-	}
-};
+//利用开发人员命令提示工具查看对象模型
+//cd 跳转文件路径
+//查看命名
+//cl /d1 reportSingleClassLayout类名 文件名（打一部分按TAB键会自动补齐）
 void test01()
 {
-	son1 s1;
-	s1.m_a = 100;
-	//s1.m_b = 100; //保护权限类外访问不到
+	//16
+	//父类中的所有非静态成员属性都会被子类继承下去
+	//父类中的私有成员属性 是被编译器给隐藏了，因此是访问不到，但是确实被继承下去了
+	cout << sizeof(son1) << endl;
 }
-
-//保护继承
-class base2
-{
-public:
-	int m_a;
-protected:
-	int m_b;
-private:
-	int m_c;
-};
-
-class son2 :protected base2
-{
-public:
-	void func()
-	{
-		m_a = 100;//父类中公共成员，到子类中变为保护权限
-		m_b = 100;//父类中保护成员，到子类中变为保护权限
-		//m_c = 100;  //父类私有成员不可访问
-	}
-};
-void test02()
-{
-	son2 s2;
-	//继承的m_a m_b全部变成保护权限，类外不可访问
-	/*s2.m_a = 100;
-	s2.m_b = 100;*/  
-}
-
-//私有继承
-class base3
-{
-public:
-	int m_a;
-protected:
-	int m_b;
-private:
-	int m_c;
-};
-class son3 :private base3
-{
-public:
-	void func()
-	{
-		//父类中公共成员和保护成员全部变成私有成员
-		m_a = 100;
-		m_b = 100;
-		//m_c = 100; //父类私有成员不可访问
-	}
-};
-class son3test : private son3
-{
-public:
-	void func()
-	{
-		//m_a = 100;//可证明son3中继承的属性已变成私有成员
-
-	}
-};
 int main()
 {
-
+	test01();
 	system("pause");
 	return 0;
 }
