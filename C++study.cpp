6b428ee9f77487a2425dda567d5d73a202271b68
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-02 18:37:43 +0800

    类和对象-函数调用符（）重载

--- C++study.cpp
+++ C++study.cpp
@@ -2,43 +2,53 @@
 #include<string>
 using namespace std;
 
-//关系运算符重载，可以让两个自定义类型的对象进行对比操作
-class person
+//函数调用符() 也可以重载
+//由于重载后使用的方式非常像函数的调用，因此称为仿函数
+//仿函数没有固定写法，非常灵活
+
+class MYprint
 {
 public:
-	person(string name, int age)
+	//重载的函数调用运算符
+	void operator()(string text)
 	{
-		m_name = name;
-		m_age = age;
+		cout << text << endl;
 	}
-	bool operator==(person &p)
-	{
-		if (this->m_name ==p.m_name&&this->m_age==p.m_age)
-		{
-			return true;
-		}
-		return false;
-	}
-	string m_name;
-	int m_age;
 };
 
 void test01()
 {
-	person p1("李四",18);
-	person p2("张三",18);
-	if (p1 == p2 )
-	{
-		cout << "p1和p2相等" << endl;
-	}
-	else
-	{
-		cout << "p1和p2不相等" << endl;
-	}
+	MYprint p;
+	p("hello world");//由于使用起来非常类似函数调用，因此称为仿函数
 }
+
+//仿函数非常灵活，没有一个固定的写法
+//加法类
+class MYadd
+{
+public:
+	int operator()(int num1, int num2)
+	{
+		return num1 + num2;
+	}
+};
+
+void test02()
+{
+	MYadd a;
+	int sum = a(10, 20);
+	cout << sum << endl;
+
+	//匿名函数对象 就是未命名的对象使用函数重载符，因为局部对象函数结束完了就释放了，无需在保留
+	cout << MYadd()(100, 200) << endl;
+}
+
+
+
 int main()
 {
-	test01();
+	//test01();
+	test02();
 	system("pause");
 	return 0;
 }
