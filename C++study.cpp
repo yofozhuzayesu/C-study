65091ea46b518500d988b71ad09d7071ef9dfa18
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-04 01:12:17 +0800

    类和对象-菱形继承
    菱形继承的解决方法-虚继承 关键字 virutal

--- C++study.cpp
+++ C++study.cpp
@@ -2,54 +2,55 @@
 #include<string>
 using namespace std;
 
-//多继承语法
-//C++允许一个类继承多个类
+//菱形继承：有两个派生类继承同一个基类  又有某个类同时继承这两个派生类  这就是菱形继承 也叫钻石继承
 
-//语法：class 子类: 继承方式 父类1，继承方式 父类2......
-//多继承可能会引发父类中有同名成员出现，需要加作用域区分
-//实际开发中不建议使用
-
-class base1
+//动物类
+class animal
 {
 public:
-	base1()
-	{
-		m_a = 100;
-	}
-	int m_a;
-};
+	int m_age;
 
-class base2
+};
+//羊类
+//在继承之前 加上关键字 virtual 变成虚继承
+//animal类称为虚基类
+class sheep:virtual public animal
 {
-public:
-	base2()
-	{
-		m_a = 200;
-	}
-	int m_a;
-};
 
-//子类需要继承base1 和 base2
-class son :public base1, public base2
+};
+//驼类
+//在继承之前 加上关键字 virtual 变成虚继承
+//animal类称为虚基类
+class tuo:virtual public animal
 {
-public:
-	son()
-	{
-		m_c = 300;
-		m_d = 400;
-	}
-	int m_c;
-	int m_d;
+
 };
 
+//羊驼类
+
+class sheeptuo : public sheep, public tuo
+{
+
+};
 
 void test01()
 {
-	son s;
-	cout << "子类son占用的内存空间" << sizeof(son) << endl;
-	//当父类中出现同名成员，需要加作用域区分
-	cout << "base1 m_a = " << s.base1::m_a << endl;
-	cout << "base2 m_a = " << s.base2::m_a << endl;
+	sheeptuo st;
+	//st.m_age = 18;  m_age出现二义性 无法明确
+	st.sheep::m_age = 18;
+	st.tuo::m_age = 28;
+
+	//第一个问题：当发生菱形继承时，两个父类拥有相同数据，需要加以作用域区分
+	//从羊类继承的m_age
+	cout << "st.sheep::m_age=" << st.sheep::m_age << endl;
+	//从驼类继承的m_age
+	cout << "st.tuo::m_age=" << st.tuo::m_age << endl;
+	//采用虚继承方式后
+	cout << "st.m_age=" << st.m_age << endl;
+
+	//但是m_age只需要一份即可，菱形继承导致数据有两份，资源浪费
+	//利用虚继承可以解决 菱形继承的问题
+	//利用虚继承 继承的是两个不同的虚基类指针以及一个变量m_age  而这个两个指针都指向同一份内存 也就是m_age变成唯一
 }
 int main()
 {
