0149d02d757ec72c477edd47a434e0024ca2d6a9
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-03 23:01:45 +0800

    类和对象-继承中的构造和析构顺序
    构造顺序：先有父类再有子类
    析构顺序相反

--- C++study.cpp
+++ C++study.cpp
@@ -2,37 +2,41 @@
 #include<string>
 using namespace std;
 
-//继承中的对象模型
-//问题：从父类继承过来的成员，哪些属于子类对象中
+//继承中构造和析构顺序
+//子类继承父类后，当创建子类对象，也会调用父类的构造函数
+//问题：父类和子类的构造和析构顺序是谁先谁后
 
 class Base
 {
 public:
-	int m_a;
-protected:
-	int m_b;
-private:
-	int m_c;
+	Base()
+	{
+		cout << "Base的构造函数" << endl;
+	}
+
+	~Base()
+	{
+		cout << "Base的析构函数" << endl;
+	}
 };
 
-//公共继承
-class son1 :public Base
+class son : public Base
 {
 public:
-	int m_d;
+	son()
+	{
+		cout << "son的构造函数" << endl;
+	}
 
+	~son()
+	{
+		cout << "son的析构函数" << endl;
+	}
 };
-
-//利用开发人员命令提示工具查看对象模型
-//cd 跳转文件路径
-//查看命名
-//cl /d1 reportSingleClassLayout类名 文件名（打一部分按TAB键会自动补齐）
 void test01()
 {
-	//16
-	//父类中的所有非静态成员属性都会被子类继承下去
-	//父类中的私有成员属性 是被编译器给隐藏了，因此是访问不到，但是确实被继承下去了
-	cout << sizeof(son1) << endl;
+	//Base b;
+	son s;
 }
 int main()
 {
