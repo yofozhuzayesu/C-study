34dbe1d5dba6025e5e0c5ccb71285de44ef0f4f8
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-06 15:26:36 +0800

    类和对象-虚析构和纯虚析构
    纯虚析构除了要声明，也必须要在类外写实现

--- C++study.cpp
+++ C++study.cpp
@@ -2,101 +2,77 @@
 #include<string>
 using namespace std;
 
-//多态案例二 制作饮品
-//大致流程：煮水-冲泡-倒入杯中-加入辅料
+//虚析构和纯虚析构
+//纯虚析构与纯虚函数不同的是：纯虚函数只需声明即可，而纯虚析构函数需要在类外写具体实现
+//有了纯虚析构之后，这个类也属于抽象类，无法实例化对象
 
-//利用多态技术实现本案例，提供抽象制作饮品基类，提供子类制作咖啡和茶叶
-
-class AbstractDrinking
+class Animal
 {
 public:
-	//煮水
-	virtual void Boil() = 0;
-	//冲泡
-	virtual void Brew() = 0;
-	//倒入杯中
-	virtual void PutInCup()= 0;
-	//辅料
-	virtual	void PutSomething() = 0;
-
-	//制作饮品
-	void makeDrink()
+	Animal()
 	{
-		Boil();
-		Brew();
-		PutInCup();
-		PutSomething();
+		cout << "Animal构造函数调用" << endl;
 	}
+
+	//利用虚析构可以解决 父类指针释放子类对象时不干净的问题
+	/*virtual ~Animal()
+	{
+		cout << "Animal的虚析构函数调用" << endl;
+	}*/
+
+	//纯虚析构的声明
+	//析构函数必须有函数的具体实现
+	//因为假设父类中有限数据开辟到堆区了，需要在析构函数中释放
+	//需要在类外写纯虚析构函数的具体实现
+	virtual ~Animal() = 0;
+
+	//纯虚函数
+	virtual void speak() = 0;
 };
-
-//制作咖啡
-class coffee :public AbstractDrinking
+//类外写纯虚析构函数的具体实现
+Animal::~Animal()
 {
-public:
-	//煮水
-	virtual void Boil()
-	{
-		cout << "煮农夫山泉" << endl;
-	}
-	//冲泡
-	virtual void Brew()
-	{
-		cout << "冲泡咖啡" << endl;
-	}
-	//倒入杯中
-	virtual void PutInCup()
-	{
-		cout << "倒入咖啡杯中" << endl;
-	}
-	//辅料
-	virtual	void PutSomething()
-	{
-		cout << "加入牛奶，糖" << endl;
-	}
-};
-//制作茶叶
-class tea :public AbstractDrinking
-{
-public:
-	//煮水
-	virtual void Boil()
-	{
-		cout << "煮百岁山" << endl;
-	}
-	//冲泡
-	virtual void Brew()
-	{
-		cout << "冲泡茶叶" << endl;
-	}
-	//倒入杯中
-	virtual void PutInCup()
-	{
-		cout << "倒入茶杯" << endl;
-	}
-	//辅料
-	virtual	void PutSomething()
-	{
-		cout << "加入柠檬" << endl;
-	}
-};
-
-
-//工作函数
-void doWork(AbstractDrinking *a)
-{
-	a->makeDrink();
-	delete a;//释放堆区数据
+	cout << "Animal的纯虚析构函数的调用" << endl;
 }
+
+class cat :public Animal
+{
+public:
+
+	//构造函数
+	cat(string name)
+	{
+		//创建在堆区，利用指针m_name去维护
+		cout << "cat的构造函数调用" << endl;
+		m_name = new string(name);
+		//需要在析构函数中，释放该数据
+	}
+
+	virtual void speak()
+	{
+		cout <<*m_name<< "小猫在说话" << endl;
+	}
+
+	//析构函数
+	~cat()
+	{
+		if (m_name != NULL)
+		{
+			cout << "cat的析构函数调用" << endl;
+			delete m_name;
+			m_name = NULL;
+		}
+	}
+	string *m_name;
+};
 
 void test01()
 {
-	//制作咖啡
-	doWork(new coffee);
-
-	cout << "--------------------" << endl;
-	//制作茶叶
-	doWork(new tea);
-
+	Animal *animal = new cat("tom");
+	animal->speak();
+	//父类指针指向子类，释放父类指针的时候不会调用子类的析构函数，导致子类如果有堆区属性出现内存泄漏
+	//解决方法：将父类中的析构函数改为虚析构
+	delete animal;
 }
 int main()
 {
