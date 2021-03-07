c5073217bae6f2e65e7b403802bd76f4120bb9ef
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-08 00:06:14 +0800

    模板案例-数组交换排序
    注意：字符型数组会在结尾自动填充一个"\0”

--- C++study.cpp
+++ C++study.cpp
@@ -1,40 +1,73 @@
 #include<iostream>
 using namespace std;
 
-//函数模板注意事项
+//函数模板案例
+/*
+  案例描述：
+  利用模板封装一个排序的函数，可以对不同数据类型数组进行排序
+  排序规则从大到小，排序算法为选择排序
+  分别利用char数组和int数组进行测试
+*/
 
 
-template<typename T> //template可以替换为class  没啥区别
+//交换的函数模板
+template<class T>
 void Myswap(T &a, T &b)
 {
 	T temp = a;
 	a = b;
 	b = temp;
 }
-void test01()
+//排序算法
+template<class T>
+void Mysort(T arr[],int len)
 {
-	int a = 20;
-	int b = 10;
-	char c = 'c';
-	//1.自动类型推导 必须推导出一致的数据类型T，才可以使用
-	//Myswap(a, c);// 错误 无法推导出一致的数据类型T
-	Myswap(a, b);
-	cout << a << endl;
-	cout << b << endl;
+	for (int  i = 0; i < len; i++)
+	{
+		int max = i;
+		//内部循环，用于找出真实的最大值
+		for (int j = i+1; j < len; j++)
+		{
+			//我们认定的最大值比遍历出的数值要小，说明j下标的元素才是真正的最大值
+			if (arr[j]>arr[max])
+			{
+				max = j;//更新最大值的下标
 
+			}
+		}
+		if (max != i)
+		{
+			//交换max和i元素
+			Myswap(arr[max], arr[i]);
+		}
+	}
 }
 
+//打印数组模板
 template<class T>
-void func()
+void printArray(T arr[], int len)
 {
-	cout << "func函数的调用" << endl;
+	for (int i = 0; i < len; i++)
+	{
+		cout << arr[i] << " " ;
+	}
+	cout << endl;
+}
+void test01()
+{
+	//测试char数组
+	char charArr[] = "badcfe";
+	int num = sizeof(charArr)/sizeof(char)-1;//数组总长度/每个字符长度  注意字符型数组在末尾自动填充\0 多占一个字节
+	Mysort(charArr, num);
+	printArray(charArr, num);
 }
 
 void test02()
 {
-	//2.模板必须要确定出T的数据类型，才可以使用
-	//func();//错误  因为无法确定T的数据类型
-	func<int>();//强制确定T的数据类型
+	int arr[] = { 1,5,9,4,3,4,6 };
+	int num = sizeof(arr) / sizeof(int);
+	Mysort(arr, num);
+	printArray(arr, num);
 }
 int main()
 {
