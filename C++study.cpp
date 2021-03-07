3bf93df82e411d8014a87eec86621250db515fc4
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-08 00:48:27 +0800

    模板的局限性

--- C++study.cpp
+++ C++study.cpp
@@ -1,54 +1,86 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 /*
-	普通函数和函数模板的调用规则:（也就是在重载的情况下）
-	1.如果函数模板和普通函数都可以实现，优先调用普通函数
-	2.可以通过空模板参数列表来强制调用函数模板
-	3.函数模板也可以发生重载
-	4.如果函数模板可以产生更好的匹配，优先调用函数模板
+	模板的局限性：
+	模板并非万能，例如用模板写一个比较的函数，但是传入的参数是两个person类的对象，就无法正常运行
+	C++为了解决这种问题，提供模板的重载，可以为这些特定的类型提供具体化的模板
 */
-
-void Myprint(int a, int b)
+class person
 {
-	cout << "这是普通函数调用" << endl;
-}
+public:
+	person(string name,int age)
+	{
+		m_name = name;
+		m_age = age;
+	}
+	string m_name;
+	int m_age;
+};
+
 
 template<class T>
-void Myprint(T a, T b)
+bool myCompare(T &a, T &b)
 {
-	cout << "这是模板函数的调用" << endl;
+	if (a == b)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
-//模板函数重载
-template<class T>
-void Myprint(T a, T b,T c)
+//利用具体化的person版本实现代码，具体化优先调用
+template<> bool myCompare(person &p1, person &p2)
 {
-	cout << "这是模板函数的调用" << endl;
+	if (p1.m_name==p2.m_name&&p1.m_age==p2.m_age)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 
 void test01()
 {
-	Myprint(10, 30);
+	int a = 10;
+	int b = 20;
+	bool ret = myCompare(a, b);
+	if (ret)
+	{
+		cout << "a=b" << endl;
+	}
+	else
+	{
+		cout << "a!=b" << endl;
+	}
 }
 
 void test02()
 {
-	//通过空模板参数列表，强制调用函数模板
-	Myprint<>(20, 60);
+	person p1("Tom", 11);
+	person p2("Tom", 10);
+	bool ret = myCompare(p1, p2);
+	if (ret)
+	{
+		cout << "p1=p2" << endl;
+	}
+	else
+	{
+		cout << "p1!=p2" << endl;
+	}
 }
-void test03()
-{
-	//理论上普通函数和函数模板都可以调用
-	//但是模板能产生更好的匹配，优先调用模板
-	Myprint('c', 'v');
-}
+
 int main()
 {
-	test01();//优先调用普通函数
-	test02();//强制调用函数模板
-	test03();
+	//test01();
+	test02();
 	system("pause");
 	return 0;
 }
