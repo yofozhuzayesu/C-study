374d10ce8ae3f9800a49a3d0fc86ed0ddefc3baa
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-07 09:41:28 +0800

    模板-模板的基本语法

--- C++study.cpp
+++ C++study.cpp
@@ -2,43 +2,73 @@
 #include<fstream>
 using namespace std;
 
-//二进制文件
-//打开方式指定为 ios:binary
+//C++另一种编程思想称为泛型编程，主要利用的技术就是模板
+//C++提供两种模板机制：函数模板和类模板
 
-//读文件
-//二进制方式写文件主要利用流对象调用成员函数read
-//函数原型:istream & read(char * buffer,int len);
-//参数解释：字符指针buffer指向内存中一段存储空间。len是读写的字节数
+//模板就是建立通用的模具，大大提高复用性
+//模板特点：模板不可以直接使用，它只是一个框架
+//模板的通用并不是万能的
 
+/*
+    函数模板作用：
+    建立一个通用函数，其函数返回值类型和形参类型可以不具体制定，用一个虚拟的类型带代表
 
-class person
+	语法：
+	template<typename T>
+	函数声明或定义
+	 
+	template-----声明创建模板
+	typename-----表明其后面的符号是一种数据类型，可以用class代替
+	T------------通用的数据类型，名称可以替换，通常为大写字母
+*/
+
+//实现两个整形交换的函数
+void swapInt(int &a, int &b)
 {
-public:
-	char m_name[64];//姓名
-	int m_age;//年龄
-};
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+//交换两个浮点型函数
+void swapDouble(double &a, double &b)
+{
+	double temp = a;
+	a = b;
+	b = temp;
+}
+
+//函数模板
+template<typename T> //声明一个模板，告诉编译器后面代码中紧跟着的T不要报错，T是一个通用数据类型
+void Myswap(T &a, T &b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
 
 void test01()
 {
-	//1.包含头文件
+	int a = 10;
+	int b = 20;
+	//利用函数交换
+	//swapInt(a, b);
 
-	//2.创建流对象
-	ifstream ifs;
+	//利用函数模板，有两种方式
+	//1.自动类型推导
+	//Myswap(a, b);
+	//2.显示指定类型
+	Myswap<int>(a, b);
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
 
-	//3.打开文件  并判断文件是否打开成功
-	ifs.open("person.txt", ios::in | ios::binary);
-	if (!ifs.is_open())
-	{
-		cout << "文件打开失败" << endl;
-	}
-	//4.读文件
-	person p;
-	ifs.read((char *)&p, sizeof(person));//强制转为char *类型
-	cout << p.m_name << endl;
-	cout << p.m_age << endl;
 
-	//5.关文件
-	ifs.close();
+
+	//double c = 3.5;
+	//double d = 6.68;
+	//swapDouble(c, d);
+	//cout << "c=" << c << endl;
+	//cout << "d=" << d << endl;
 }
 int main()
 {
