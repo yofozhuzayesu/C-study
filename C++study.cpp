37d7eec33609c7b32e2586858a3fcb723ec136d6
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-08 16:51:25 +0800

    类模板成员函数类外继承

--- C++study.cpp
+++ C++study.cpp
@@ -3,47 +3,56 @@
 using namespace std;
 
 /*
-	类模板与继承，需要注意以下几点
-	1.当子类继承的父类是一个类模板是，子类在声明的时候，要指定出父类中T的类型
-	2.如果不指定，编译器无法给子类分配内存
-	3.如果想灵活指定父类中T的类型，子类也需要变为模板
-
+	类模板成员函数类外实现
 */
 
-template<class T>
-class Base
-{
-	T m;
-};
-//class son :public Base//错误，必须要知道父类中的T类型，才能给子类
-class Son:public Base<int>
-{
-
-};
-
-//如果想灵活指定父类中T的类型，子类也需要变为模板
 template<class T1,class T2>
-class Son2 :public Base<T2>
+class Person
 {
 public:
-	Son2()
-	{
-		cout << "T1的类型为：" << typeid(T1).name() << endl;
-		cout << "T2的类型为：" << typeid(T2).name() << endl;
+	//类内实现
+	Person(T1 name, T2 age);
+	//{
+	//	this->m_name = name;
+	//	this->m_age = age;
+	//}
 
-	}
-	T1 obj;
+
+	//类内实现
+	void showPerson();
+	//{
+	//	cout << this->m_name << endl;
+	//	cout << this->m_age << endl;
+	//}
+
+
+	T1 m_name;
+	T2 m_age;
 };
-void test02()
+//构造函数类外实现
+template<class T1,class T2>
+Person<T1,T2>::Person(T1 name, T2 age)
 {
-	//Son2 s2;   错误 因为 Son2 也是模板 也需要指定模板参数类型
-	Son2<int, char> s2;
-
+	this->m_name = name;
+	this->m_age = age;
 }
 
+//成员函数类外实现
+template<class T1,class T2>
+void Person<T1, T2>::showPerson()
+{
+	cout << this->m_name << endl;
+	cout << this->m_age << endl;
+}
+
+void test01()
+{
+	Person<string, int> p("孙悟空", 999);
+	p.showPerson();
+}
 int main()
 {
-	test02();
+	test01();
 	system("pause");
 	return 0;
 }
