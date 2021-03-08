d98196ea6e43ef4832f3fc169df1f842fcc2b3f9
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-08 14:55:15 +0800

    类模板与函数模板的区别

--- C++study.cpp
+++ C++study.cpp
@@ -3,45 +3,48 @@
 using namespace std;
 
 /*
-	类模板作用
-	建立一个通用类，类中的成员 数据类型可以不具体制定，用一个虚拟的类型来代表
-
-	语法：
-	template<typename T>
-	类
+	类模板和函数模板的区别：
+	1.类模板没有自动类型推导的使用方式
+	2.类模板在模板参数列表中可以有默认参数,函数模板中不允许使用
 */
 
-//类模板
-template<class NameType,class AgeType>
+//使AgeType 默认为int 型
+template<class NameType,class AgeType = int>
 class person
 {
 public:
-	person(NameType name, AgeType age)
+	person(NameType name,AgeType age)
 	{
 		this->m_name = name;
 		this->m_age = age;
 	}
-	void showPerson()
+	NameType m_name;
+	AgeType m_age;
+	void showName()
 	{
 		cout << this->m_name << endl;
 		cout << this->m_age << endl;
 	}
-	string m_name;
-	int m_age;
 };
 
 void test01()
 {
-	//<string,int>是模板参数列表 用来个NameType 和 AgeType来传值
-	//("孙悟空", 999)则是传给了构造函数
-	person<string, int> p1("孙悟空", 999);
-	p1.showPerson();
+	//person p1("孙悟空",9999); //无法使用自动类型推导，
+	//需要用显式指定类型
+	person<string, int> p1("孙悟空", 99999);
+	p1.showName();
 }
 
-
+void test02()
+{
+	//模板中已设置默认参数，可以省去模板参数列表中的int
+	person<string> p2("猪八戒", 999);
+	p2.showName();
+}
 int main()
 {
 	test01();
+	test02();
 	system("pause");
 	return 0;
 }
