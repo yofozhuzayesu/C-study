4cd9ba894e64ca6bba8faf255024865173e88beb
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-08 16:42:28 +0800

    类模板与继承

--- C++study.cpp
+++ C++study.cpp
@@ -3,77 +3,47 @@
 using namespace std;
 
 /*
-	类模板对象做函数参数
-	类模板实例化出的对象，向函数传参的方式
+	类模板与继承，需要注意以下几点
+	1.当子类继承的父类是一个类模板是，子类在声明的时候，要指定出父类中T的类型
+	2.如果不指定，编译器无法给子类分配内存
+	3.如果想灵活指定父类中T的类型，子类也需要变为模板
 
-	一共有三种传入方式
-	1.指定传入的类型 --直接显示对象的数据类型
-	2.参数模板化     --将对象中的参数变为模板进行传递
-	3.整个类模板化   --将这个对象类型 模板化进行传递
 */
 
-template<class T1,class T2>
-class person
+template<class T>
+class Base
 {
-public:
-	person(T1 name, T2 age)
-	{
-		this->m_name = name;
-		this->m_age = age;
-	}
-	T1 m_name;
-	T2 m_age;
-	void showPerson()
-	{
-		cout << this->m_name << endl;
-		cout << this->m_age << endl;
-	}
+	T m;
+};
+//class son :public Base//错误，必须要知道父类中的T类型，才能给子类
+class Son:public Base<int>
+{
+
 };
 
-//1.指定传入类型  也就是指定person类模板中参数列表为<string,int>类型
-void printPerson1(person<string,int> &p)
-{
-	p.showPerson();
-}
-void test01()
-{
-	person<string, int> p1("孙悟空", 999);
-	//类模板对象
-	printPerson1(p1);
-}
-
-//2.参数模板化  告诉函数 形参中的T1 T2是模板中的两个参数
+//如果想灵活指定父类中T的类型，子类也需要变为模板
 template<class T1,class T2>
-void printPerson2(person<T1,T2> &p)
+class Son2 :public Base<T2>
 {
-	p.showPerson();
-	//利用 typeid查看模板参数类型
-	cout << "T1的类型=" << typeid(T1).name() << endl;
-	cout << "T2的类型=" << typeid(T2).name() << endl;
-}
+public:
+	Son2()
+	{
+		cout << "T1的类型为：" << typeid(T1).name() << endl;
+		cout << "T2的类型为：" << typeid(T2).name() << endl;
+
+	}
+	T1 obj;
+};
 void test02()
 {
-	person<string, int> p2("猪八戒", 888);
-	printPerson2(p2);
+	//Son2 s2;   错误 因为 Son2 也是模板 也需要指定模板参数类型
+	Son2<int, char> s2;
+
 }
 
-//3.将整个类都模板化 让编译器自动识别
-template<class T>
-void printPerson3(T &p)
-{
-	p.showPerson();
-	cout << "T的数据类型" << typeid(T).name() << endl;
-}
-void test03()
-{
-	person<string, int> p3("沙僧", 777);
-	printPerson3(p3);
-}
 int main()
 {
-	//test01();
-	//test02();
-	test03();
+	test02();
 	system("pause");
 	return 0;
 }
