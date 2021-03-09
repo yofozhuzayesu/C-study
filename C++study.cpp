a4079a19e32960bede5873c6db50d059445cea92
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-09 18:30:45 +0800

    类模版与友元
    全局函数在类外实现非常复杂，在类内实现最简单

--- C++study.cpp
+++ C++study.cpp
@@ -1,32 +1,79 @@
-#pragma once
 #include<iostream>
-
+#include<string>
 using namespace std;
 
-#include<string>
+/*
+
+  掌握类模板配合友元函数的类内和类外实现
+
+  全局函数类内实现-直接在类内声明友元即可
+  全局函数类外实现-需要提前让编译器知道全局函数的存在
+
+*/
+
+//提前让编译器知道Person类的存在
+template<class T1,class T2>
+class person;
+
+//类外实现
+//函数模板的实现
+//所以在上面的声明中需要写明是函数模板的声明
+template<class T1, class T2>
+void printPerson2(person<T1, T2>p)
+{
+	cout << p.m_name << endl;
+	cout << p.m_age << endl;
+}
+
+
+//通过全局函数打印person信息
 template<class T1, class T2>
 class person
 {
+	//全局函数类内实现
+	friend void printPerson(person<T1, T2>p)
+	{
+		cout << p.m_name << endl;
+		cout << p.m_age << endl;
+	}
+
+	//全局函数类外实现
+	//类内写声明
+	//普通函数的声明，不是类内的函数
+	//加空模板的参数列表
+	//如果全局函数是类外实现，需要让编译器提前知道这个函数的存在
+	friend void printPerson2<>(person<T1, T2>p);
 
 public:
-	//类内声明 类外实现
-	person(T1 name, T2 age);
-	void showperson();
-
+	person(T1 name, T2 age)
+	{
+		this->m_name = name;
+		this->m_age = age;
+	}
+private:
 	T1 m_name;
 	T2 m_age;
 };
 
-template<class T1, class T2>
-person<T1, T2>::person(T1 name, T2 age)
+
+
+//全局函数类内实现的测试
+void test01()
 {
-	this->m_name = name;
-	this->m_age = age;
+	person<string, int> p("孙悟空", 999);
+	printPerson(p);
 }
 
-template<class T1, class T2>
-void person<T1, T2>::showperson()
+//全局函数类外实现的测试
+void test02()
 {
-	cout << this->m_name << endl;
-	cout << this->m_age << endl;
+	person<string, int> p2("孙悟空", 9999);
+	printPerson2(p2);
+}
+int main()
+{
+	//test01();
+	test02();
+	system("pause");
+	return 0;
 }
