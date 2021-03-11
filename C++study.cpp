a3ba17dd0397268ae6af8d8a612c1d37f4db048c
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-12 00:08:46 +0800

    string容器-字符串比较

--- C++study.cpp
+++ C++study.cpp
@@ -2,41 +2,36 @@
 #include<string>
 using namespace std;
 
-//string查找和替换
 
+//string 字符串比较
 /*
-	功能描述：
-	查找：查找指定字符串是否存在
-	替换：在指定的位置替换字符串
-
+	比较方式：是按字符的ASCII码进行对比
+	 = 返回0
+	 > 返回1
+	 < 返回-1
 */
 
-//1.查找
 void test01()
 {
-	string str1 = "abcdefghjklmn";
-	int pos = str1.find("de");
-	cout << pos << endl;//pos为3 表示从第四号位开始    查找不到的时候就返回-1 （必须要用一个整形变量接收）
+	string str1 = "hello";
+	string str2 = "hello";
 
-	//rfind
-	//rfind从右往左查找（也就是从右边开始有更靠前的de的时候就查找该de），find从左往右查找
-	//虽热两个查找方式不同，但是输出结果都是从左往右的
-	pos = str1.rfind("de");
-	cout << pos << endl;
-}
-
-
-//替换
-void test02()
-{
-	string str1 = "abcdefg";
-	str1.replace(1, 3, "1111");//表示从1号位置开始，往后三个字符替换成"1111"
-	cout << str1 << endl;
+	if (str1.compare(str2)==0)
+	{
+		cout << "str1 = str2" << endl;
+	}
+	else if (str1.compare(str2)>0)
+	{
+		cout << "str1 > str2" << endl;
+	}
+	else
+	{
+		cout << "str1 < str2" << endl;
+	}
 }
 int main()
 {
-	//test01();
-	test02();
+	test01();
 	system("pause");
 	return 0;
 }
