9d61d22466d6706ec86cf767e981734665342782
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-11 22:07:16 +0800

    STL初识-Vector容器存放自定义数据类型

--- C++study.cpp
+++ C++study.cpp
@@ -4,25 +4,6 @@
 #include<algorithm>
 using namespace std;
 
-
-/*
-    STL基本概念
-	1.STL：标准模板库
-	2.STL从广义上分为容器（container）算法(algorithm)迭代器(itreator)
-	3.容器和算法之间通过迭代器进行无缝连接
-	4.STL几乎所有的代码都采用了模板类或者模板函数
-*/
-
-/*
-	STL六大组件：容器、算法、迭代器、仿函数、适配器（配接器）、空间配置器
-	1.容器：各种数据结构，如vector、list、deque、set、map等，用来存放数据
-	2.算法：各种常用的算法，如sort、find、copy、for_each等
-	3.迭代器：扮演了容器与算法之间的胶合剂
-	4.仿函数：行为类似函数，可作为算法的某种策略
-	5.适配器：一种用来修饰容器或者仿函数或迭代器接口的东西
-	6.空间配置器：负责空间的配置与管理
-*/
-
 /*
 
 	容器：vector
@@ -30,55 +11,85 @@ using namespace std;
 	迭代器:vector<int>::iterator
 */
 
+//vector容器 存放自定义数据类型
 
-void printArr(int val)
+
+
+
+//自定义数据类型
+class person
 {
-	cout << val << " ";
-}
-//vector容器存放内置数据类型
-void test01()
-{	//创建了一个vector容器
-	vector<int> v;
-
-	//向容器中插入数据
-	v.push_back(10);
-	v.push_back(20);
-	v.push_back(30);
-	v.push_back(40);
-	v.push_back(50);
-
-	//访问容器中的数据
-	/*for (int i = 0; i < v.size(); i++)
+public:
+	person(string name,int age)
 	{
-		cout << v[i] << " ";
+		this->m_name = name;
+		this->m_age = age;
 	}
-	cout << endl;*/
+	string m_name;
+	int m_age;
+};
 
-	//通过迭代器访问容器中的数据
-	//vector<int>::iterator itBegin = v.begin();//起始迭代器，指向容器中的第一个元素
-	//vector<int>::iterator itEnd = v.end();//结束迭代器，指向容器中最后一个元素的下一个位置
+//ostream &operator <<(ostream &cout, person p)
+//{
+//	cout << p.m_name << "  " << p.m_name << endl;
+//	return cout;
+//}
 
-	//第一种遍历方式
-	//while (itBegin != itEnd)
-	//{
-	//	cout << *itBegin << " ";
-	//	itBegin++;
-	//}
+void test01()
+{
+	person p1("孙悟空", 99999);
+	person p2("孙悟空", 99999);
+	person p3("孙悟空", 99999);
+	person p4("孙悟空", 99999);
 
-	//第二种遍历方式
-	//for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
-	//{
-	//	cout << *it << " ";
+	vector<person> v;
+	v.push_back(p1);
+	v.push_back(p2);
+	v.push_back(p3);
+	v.push_back(p4);
 
-	//}
-	//cout << endl;
+	for (vector<person>::iterator i = v.begin(); i != v.end(); i++)
+	{
+		//一种输出方式是重载<<符号
+		//cout << *i << endl;
 
-	//第三种遍历方式  利用STL中提供的遍历算法
-	for_each(v.begin(), v.end(), printArr);
+		//第二种输出方式  i的解引用就是person类对象，然后直接访问对象内中的属性即可
+		//cout << (*i).m_name << " " << (*i).m_age << endl;
+
+		//第三种输出方式
+		//i是指针，可以通过指针的方式拿到数据
+		cout << i->m_name << " " << i->m_age << endl;
+	}
 }
+
+//存放自定义类型指针
+void test02()
+{
+	person p1("孙悟空", 99999);
+	person p2("孙悟空", 99999);
+	person p3("孙悟空", 99999);
+	person p4("孙悟空", 99999);
+
+	vector<person*> v;
+	v.push_back(&p1);
+	v.push_back(&p2);
+	v.push_back(&p3);
+	v.push_back(&p4);
+
+	for (vector<person*>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		//第一种输出方式，先解引用出对象
+		//cout << (*(*it)).m_name << " " << (*(*it)).m_age << endl;
+
+		//第二种方式，*it 也是指针
+		cout << (*it)->m_name << " " << (*it)->m_age << endl;
+	}
+}
+
 int main()
 {
-	test01();
+	//test01();
+	test02();
 	system("pause");
 	return 0;
 }
