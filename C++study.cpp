0eee48c4b617b4408b20d762058e1a34e841f391
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-12 17:21:39 +0800

    Deque容器-赋值操作

--- C++study.cpp
+++ C++study.cpp
@@ -3,25 +3,16 @@
 using namespace std;
 
 /*
-	deque容器
-	双端数组，可以对头端进行插入删除杀出
-
-	deque和vector区别
-	1.vector对于头部的插入删除效率低，数据量大，效率越低
-	2.deque相对而言，对头部的插入删除速度比vector快
-	3.vector访问元素时的速度会比deque快，这和两者内部实现有关
-
-	deque构造函数 类似vector
+	deque容器赋值操作
+	deque& operator=(const deque &deq)            //重载等号操作符
+	assign(beg,end)                               //将[beg,end)区间中的数据拷贝赋值给本身
+	assign(n,elem)                                //将n个elem拷贝赋值给本身
 */
 
 void printDeque(const deque<int>&d)
 {
 	for (deque<int>::const_iterator it = d.begin(); it != d.end(); it++)
 	{
-		//为了防止修改操作
-		//形参中用const 
-		//同样的迭代器 也要改写成const_iterator
-		//*it = 100;
 		cout << *it << " ";
 	}
 	cout << endl;
@@ -29,22 +20,23 @@ void printDeque(const deque<int>&d)
 void test01()
 {
 	deque<int> d1;
-	for (int i = 0; i < 10; i++)
+	for (int i  = 0; i < 10; i++)
 	{
 		d1.push_back(i);
 	}
 	printDeque(d1);
 
-	deque<int>d2(d1.begin(), d1.end());//将d1中的区间内的元素拷贝给d2
+	deque<int> d2 = d1;
 	printDeque(d2);
 
-	deque<int>d3(10, 100);
+	deque<int> d3;
+	d3.assign(d2.begin(), d2.begin() + 3);
 	printDeque(d3);
 
-	deque<int>d4(d3);
+	deque<int> d4;
+	d4.assign(5, 10);
 	printDeque(d4);
 }
-
 int main()
 {
 	test01();
