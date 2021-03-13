b8f95b1ec1237df6269d776e79fd137029eb4013
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-13 22:47:32 +0800

    list容器-数据存储

--- C++study.cpp
+++ C++study.cpp
@@ -3,19 +3,13 @@
 using namespace std;
 
 /*
-	list 插入和删除
+	list容器 数据存取
+	对list容器中数据进行存取
+	front()  //返回第一个元素
+	back()   //返回最后一个元素
 
-	push_back(elem)
-	pop_back()
-	push_front(elem)
-	pop_front
-	insert(pos.elem);//在pos位置插入elem元素的拷贝，返回新数据的位置
-	insert(pos,n,elem);//在pos位置插入n个elem数据，无返回值
-	insert(pos,beg,end);//在pos位置插入[beg,end)区间的数据，无返回值
-	clear()
-	erase(beg,end)
-	erase(pos);//删除pos位置的数据，返回下一个数据的位置
-	remove(elem);//删除容器中所有与elem值匹配的元素
+	list不支持at和[]  因为链表不是连续的存储空间
+	迭代器也不支持随机（也就是跳跃式）访问  ---即不支持 list ::iterator+2操作 只支持++ --
  */
 
 void printList(const list<int>&L)
@@ -34,30 +28,9 @@ void test01()
 	l.push_back(20);
 	l.push_back(30);
 	l.push_back(40);
-
-	//头插
-	l.push_front(50);
-	printList(l);//50 10 20 30 40
-	
-	//insert插入
-	list<int>::iterator it = l.begin();
-	l.insert(++it, 1000);
-	printList(l);//50 1000 10 20 30 40
-
-	//删除
-	it = l.begin();
-	l.erase(++it);
-	printList(l);// 50 10 20 30 40
-
-	//移除
-	l.push_back(10000);
-	l.push_back(10000);
-	l.push_back(10000);
-	cout << "移除之前" << endl;
 	printList(l);
-	cout << "移除之后" << endl;
-	l.remove(10000);//删除同样的所有的所有数据
-	printList(l);
+	cout << "L的第一个元素为" << l.front() << endl;
+	cout << "L的最后一个元素为" << l.back() << endl;
 }
 
 
