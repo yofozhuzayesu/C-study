a5f9bc8fdd7e7b44b463fec01395986c8c24edd1
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-13 22:40:40 +0800

    List容器-插入和删除

--- C++study.cpp
+++ C++study.cpp
@@ -3,12 +3,19 @@
 using namespace std;
 
 /*
-	list大小操作
-	size()
-	empty()
-	resize(num)
-	resize(num,elem)
+	list 插入和删除
 
+	push_back(elem)
+	pop_back()
+	push_front(elem)
+	pop_front
+	insert(pos.elem);//在pos位置插入elem元素的拷贝，返回新数据的位置
+	insert(pos,n,elem);//在pos位置插入n个elem数据，无返回值
+	insert(pos,beg,end);//在pos位置插入[beg,end)区间的数据，无返回值
+	clear()
+	erase(beg,end)
+	erase(pos);//删除pos位置的数据，返回下一个数据的位置
+	remove(elem);//删除容器中所有与elem值匹配的元素
  */
 
 void printList(const list<int>&L)
@@ -21,34 +28,36 @@ void printList(const list<int>&L)
 }
 void test01()
 {
-	list<int>l1;
+	list<int>l;
+	//尾插
+	l.push_back(10);
+	l.push_back(20);
+	l.push_back(30);
+	l.push_back(40);
 
-	//添加数据
-	l1.push_back(10);
-	l1.push_back(20);
-	l1.push_back(30);
-	l1.push_back(40);
-
-	//遍历容器
-	printList(l1);
-
-	//size() 和empty()
-	if (l1.empty())
-	{
-		cout << "l1容器为空" << endl;
-	}
-	else
-	{
-		cout << "l1容器不为空" << endl;
-	}
-	cout << "l1中的元素个数为：" << l1.size() << endl;
+	//头插
+	l.push_front(50);
+	printList(l);//50 10 20 30 40
 	
-	//resize
-	l1.resize(6);
-	printList(l1);//默认用0填充
-	l1.resize(15, 2);//指定用2填充
-	printList(l1);
+	//insert插入
+	list<int>::iterator it = l.begin();
+	l.insert(++it, 1000);
+	printList(l);//50 1000 10 20 30 40
 
+	//删除
+	it = l.begin();
+	l.erase(++it);
+	printList(l);// 50 10 20 30 40
+
+	//移除
+	l.push_back(10000);
+	l.push_back(10000);
+	l.push_back(10000);
+	cout << "移除之前" << endl;
+	printList(l);
+	cout << "移除之后" << endl;
+	l.remove(10000);//删除同样的所有的所有数据
+	printList(l);
 }
 
 
