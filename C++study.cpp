294edc930361c73958e43cc8774cfc3f822bd176
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-13 23:49:15 +0800

    set容器-构造和赋值

--- C++study.cpp
+++ C++study.cpp
@@ -1,75 +1,50 @@
 #include<iostream>
-#include<list>
-#include<ctime>
-#include<string>
+#include<set>
 using namespace std;
 
 /*
-	排序案例
-	案例描述：将person自定义数据类型进行排序，Person中属性有姓名、年龄、身高
-	排序规则：按照年龄进行升序，如果年龄相同按照身高进行降序
+	set/multiset容器
+	所有元素都会在插入的时自动排序
+
+	本质：属于关联式容器，底层结构是用二叉树实现
+
+	set/multiset区别
+	set不允许容器中有重复的元素
+	multiset允许容器中有重复的元素
+
+	构造：
+	set<T> st;
+	set(const set &st)
+	赋值：
+	set& operator=(const set &st)
 
  */
 
-class person
+void printSet(const set<int>st)
 {
-public:
-	person() {};
-	person(string name,int age,int height)
+	for (set<int>::iterator it =st.begin(); it != st.end(); it++)
 	{
-		this->m_name = name;
-		this->m_height = height;
-		this->m_age = age;
+		cout << *it << " ";
 	}
-	string m_name;
-	int m_age;
-	int m_height;
-};
-
-//遍历输出
-void printList(const list<person>l)
-{
-	for (list<person>::const_iterator it = l.begin(); it != l.end(); it++)
-	{
-		cout << (*it).m_name << "  " << (*it).m_age << "  " << (*it).m_height << endl;
-	}
-}
-
-//指定排序规则
-bool myCompare(person &p1, person &p2)
-{
-	//按照年龄升序
-	if (p1.m_age==p2.m_age)
-	{
-		//年龄相同的情况下  按身高降序
-		return p1.m_height > p2.m_height;
-	}
-	return p1.m_age < p2.m_age;
+	cout << endl;
 }
 void test01()
 {
-	list<person>lst;
-	person p1("张三", 23, 178);
-	person p2("李四", 45, 196);
-	person p3("王五", 23, 187);
-	person p4("陈六", 19, 185);
-	person p5("徐七", 20, 177);
-	person p6("刘八", 20, 179);
+	set<int>s1;
 
-	lst.push_back(p1);
-	lst.push_back(p2);
-	lst.push_back(p3);
-	lst.push_back(p4);
-	lst.push_back(p5);
-	lst.push_back(p6);
-	cout << "排序前：" << endl;
-	printList(lst);
+	//set插入数据 只有insert方式
+	//插入的时候就自动升序排序了  并且不会出现重复值
+	s1.insert(30);
+	s1.insert(30);
+	s1.insert(20);
+	s1.insert(10);
+	s1.insert(40);
 
-	//排序
-	lst.sort(myCompare);
-	cout << "****************************" << endl;
-	cout << "排序后：" << endl;
-	printList(lst);
+	printSet(s1);
+
+	//拷贝构造
+	set<int>s2(s1);
+	printSet(s2);
 }
 
 int main()
