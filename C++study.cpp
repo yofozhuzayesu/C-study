da6d83219b6d77d7cc325827fea3bbd125655557
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-15 01:06:02 +0800

    map容器-排序

--- C++study.cpp
+++ C++study.cpp
@@ -3,37 +3,38 @@
 using namespace std;
 
 /*
-	查找和统计
-	find(key)      //查找key是否存在，若存在，返回该键的元素的迭代器；若不存在，返回set.end()
-	count(key)     //统计key的元素个数 对于map容器而言 结果为0或1
+	map容器默认排序规则 是按照key值进行从小到大排序
+	利用防函数，可以改变排序规则
+
 */
-void printMap(const map<int, int> &m)
+class myCompare
 {
-	for (map<int, int>::const_iterator it = m.begin(); it != m.end(); it++)
+public:
+	bool operator()(int v1, int v2)
+	{
+		//降序
+		return v1 > v2;
+	}
+};
+
+void printMap(const map<int, int,myCompare> &m)
+{
+	for (map<int, int,myCompare>::const_iterator it = m.begin(); it != m.end(); it++)
 	{
 		cout << "key值:" << (*it).first << "  " << "Value:" << (*it).second << endl;
 	}
 }
 void test01()
 {
-	map<int, int>mp1;
+	//将map的第三个数据类型改为自定义的类， 即可按照降序排列
+	map<int, int,myCompare>mp1;
 	
 	mp1.insert(pair<int, int>(1, 10));
 	mp1.insert(pair<int, int>(4, 60));
 	mp1.insert(pair<int, int>(7, 50));
 	mp1.insert(pair<int, int>(3, 60));
 
-	//查找key=3的数据
-	map<int, int>::iterator it = mp1.find(9);
-	if (it != mp1.end())
-	{
-		cout << "找到了" << endl;
-		cout << "key值为：" << (*it).first << "   " << "value:" << (*it).second << endl;
-	}
-	else
-	{
-		cout << "未找到" << endl;
-	}
+	printMap(mp1);
 	
 }
 
