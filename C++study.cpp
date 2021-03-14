da15395dac228e5be58af1511fd8537b2ea53432
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-14 23:49:12 +0800

    pair对组的创建

--- C++study.cpp
+++ C++study.cpp
@@ -1,74 +1,35 @@
 #include<iostream>
-#include<set>
+#include<string>
 using namespace std;
 
 /*
-	set和multiset区别
-	set不可以插入重复数据，而multiset可以
-	set插入数据的同时会返回插入结果，表示插入是否成功
-	multiset不会检测数据，因此可以插入重复数据
+	pair对组创建
+	成对初夏你的数据，利用对组可以返回两个数据
+
+	两种创建方式：
+	pair<type,type> p (value1,value2)     //默认构造
+	pair<type,type> p = make_pair(value1,value2)
  */
 
-void printSet(const set<int>st)
-{
-	for (set<int>::iterator it = st.begin(); it != st.end(); it++)
-	{
-		cout << *it << " ";
-	}
-	cout << endl;
-}
 
-void printMultiset(const multiset<int> ms)
-{
-	for (multiset<int>::const_iterator it = ms.begin(); it != ms.end(); it++)
-	{
-		cout << *it << " ";
-	}
-	cout << endl;
-}
 void test01()
 {
-	set<int>s1;
-	pair<set<int>::iterator,bool> ret =  s1.insert(10);
-	if (ret.second)
-	{
-		cout << "第一次插入成功" << endl;
-	}
-	else
-	{
-		cout << "第一次插入失败" << endl;
-	}
+	//创建对组
+	pair<string, int> p("孙悟空", 9999);
+	//获取对组属性
+	cout << p.first << endl;
+	cout << p.second << endl;
 
-	pair<set<int>::iterator, bool> ret2 = s1.insert(10);
-	if (ret2.second)
-	{
-		cout << "第二次插入成功" << endl;
-	}
-	else
-	{
-		cout << "第二次插入失败" << endl;
-	}
 
-	
+	//第二种方式创建对组
+	pair<string, int> p2 = make_pair("猪八戒", 999999);
+	cout << p2.first << endl;
+	cout << p2.second << endl;
 }
 
-void test02()
-{
-	//multiset允许重复插入
-	multiset<int> ms1;
-	ms1.insert(10);
-	ms1.insert(10);
-	printMultiset(ms1);
-
-	//multiset的insert函数返回的是一个迭代器
-	multiset<int>::iterator it = ms1.insert(400);
-	cout << *it << endl;
-
-}
 int main()
 {
-	//test01();
-	test02();
+	test01();
 	system("pause");
 	return 0;
 }
