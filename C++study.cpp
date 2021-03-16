146db5b0f99d69773b08302957bcd8ee3802d221
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-16 10:25:02 +0800

    一元谓词

--- C++study.cpp
+++ C++study.cpp
@@ -1,78 +1,48 @@
 #include<iostream>
 #include<string>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 /*
-	函数对象概念
-	重载了函数调用操作符的类，其对象常称为函数对象
-	函数对象使用重载（）时，行为类似函数调用，也叫仿函数
-
-	本质：函数对象(仿函数)是一个类，不是一个函数
-
-	特点：
-	函数对象在使用时，可以像普通函数那样调用，可以有参数，可以有返回值
-	函数对象超出普通函数的概念，函数对象可以有自己的状态
-	函数对象可以作为参数传递
+	谓词概念
+	返回bool类型的仿函数称为谓词
+	如果operator()接受一个参数，那么叫做一元谓词
+	如果operator()接受两个参数，那么叫做二元谓词
 
 */
-
-//函数和对象在使用时，可以像普通函数那样调用，可以有参数，可以有返回值
-class myAdd
+class Five
 {
 public:
-	int operator()(int v1, int v2)
+	bool operator()(int val)
 	{
-		return v1 + v2;
+		return val > 5;
 	}
 };
-
 void test01()
 {
-	myAdd m1;
-	cout << m1(10, 90) << endl;
-}
-
-//函数对象超出普通函数的概念，函数对象可以有自己的状态
-class myPrint
-{
-public:
-	myPrint()
+	vector<int>v;
+	for (int  i = 0; i < 10; i++)
 	{
-		this->count=0;
+		v.push_back(i);
 	}
-	void operator()(string str)
+
+	//需求：在容器中查找有没有大于5的数字
+	//需要使用到find_if算法  返回值是找到的第一个大于5的元素的迭代器
+	vector<int>::const_iterator it= find_if(v.begin(), v.end(), Five());//第三个参数是谓词，应该传入一个函数对象，但是用匿名函数对象也可以
+	if (it != v.end())
 	{
-		cout << str << endl;
-		this->count++;
+		cout << "找到了大于5的数字为" << (*it) << endl;
 	}
-	int count;//内部自己的状态
-};
-
-void test02()
-{
-	myPrint m;
-	m("chenxing");
-	m("chenxing");
-	m("chenxing");
-	m("chenxing");
-	cout<<"myprint调用次数" << m.count << endl;
+	else
+	{
+		cout << "未找到" << endl;
+	}
 }
 
-//函数对象可以作为参数传递
-void doPrint(myPrint &mp,string str)
-{
-	mp(str);
-}
-void test03()
-{
-	myPrint myprint;
-	doPrint(myprint,"chenxing");
-}
 int main()
 {
-	//test01();
-	//test02();
-	test03();
+	test01();
 	system("pause");
 	return 0;
 }
