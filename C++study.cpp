577afc98bdaa48fb4af63d386248878a650cd096
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-17 00:33:17 +0800

    for_each算法

--- C++study.cpp
+++ C++study.cpp
@@ -1,45 +1,52 @@
 #include<iostream>
-#include<functional>
 #include<vector>
 #include<algorithm>
 using namespace std;
 
 /*
-	逻辑仿函数：实现逻辑运算
-	template<class T> bool logical_and<T>               //逻辑与
-	template<class T> bool logical_or<T>                //逻辑或
-	template<class T> bool logical_not<T>               //逻辑非
+	STL常用算法
+	算法主要是由头文件<algorothm><functional><numeric>组成
+	<algorithm>是所有头文件中最大的一个，范围涉及到比较、交换、查找、遍历操作、复制、修改等等
+	<numeric>体积很小，只包括几个在序列上面进行简单数学运算你的模板函数
+	<functional>定义了一些模板类，用以声明函数对象
 
 */
-void printVector(vector<bool> v)
+
+/*
+	遍历算法  遍历容器元素
+	for_each(iterator beg,iterator end,_func)
+	三个参数分别表示 开始迭代器 结束迭代器  _func函数或者函数对象
+
+*/
+
+//普通函数
+void print01(int val)
 {
-	for (vector<bool>::iterator i = v.begin(); i!= v.end(); i++)
-	{
-		cout << *i << "   ";
-	}
-	cout << endl;
+	cout << val << " ";
 }
 
-//测试非
+//class对象
+class print02
+{
+public:
+	void operator()(int val)
+	{
+		cout << val << " ";
+	}
+};
 void test01()
 {
-	vector<bool>v;
-	v.push_back(true);
-	v.push_back(false);
-	v.push_back(true);
-	v.push_back(false);
-	
-	printVector(v);
+	vector<int>v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+	}
+	//普通函数
+	for_each(v.begin(), v.end(),print01);
 
-	//利用逻辑非  将容器V搬运到容器V2中，并执行取反操作
-	vector<bool>v2;
-	v2.resize(v.size());
-
-	//搬运算法 transform  前两个参数是原容器的起止地址，第三个参数是新容器的起地址
-	transform(v.begin(), v.end(), v2.begin(), logical_not<bool>());
-	printVector(v2);
-	
-
+	cout << endl;
+	//对象
+	for_each(v.begin(), v.end(), print02());
 }
 int main()
 {
