2aaa2a2c94fa6bde333ea9772087f86886e5e4ee
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-16 10:13:57 +0800

    函数对象的基本使用

--- C++study.cpp
+++ C++study.cpp
@@ -1,46 +1,78 @@
 #include<iostream>
-#include<map>
+#include<string>
 using namespace std;
 
 /*
-	map容器默认排序规则 是按照key值进行从小到大排序
-	利用防函数，可以改变排序规则
+	函数对象概念
+	重载了函数调用操作符的类，其对象常称为函数对象
+	函数对象使用重载（）时，行为类似函数调用，也叫仿函数
+
+	本质：函数对象(仿函数)是一个类，不是一个函数
+
+	特点：
+	函数对象在使用时，可以像普通函数那样调用，可以有参数，可以有返回值
+	函数对象超出普通函数的概念，函数对象可以有自己的状态
+	函数对象可以作为参数传递
 
 */
-class myCompare
+
+//函数和对象在使用时，可以像普通函数那样调用，可以有参数，可以有返回值
+class myAdd
 {
 public:
-	bool operator()(int v1, int v2)
+	int operator()(int v1, int v2)
 	{
-		//降序
-		return v1 > v2;
+		return v1 + v2;
 	}
 };
 
-void printMap(const map<int, int,myCompare> &m)
-{
-	for (map<int, int,myCompare>::const_iterator it = m.begin(); it != m.end(); it++)
-	{
-		cout << "key值:" << (*it).first << "  " << "Value:" << (*it).second << endl;
-	}
-}
 void test01()
 {
-	//将map的第三个数据类型改为自定义的类， 即可按照降序排列
-	map<int, int,myCompare>mp1;
-	
-	mp1.insert(pair<int, int>(1, 10));
-	mp1.insert(pair<int, int>(4, 60));
-	mp1.insert(pair<int, int>(7, 50));
-	mp1.insert(pair<int, int>(3, 60));
-
-	printMap(mp1);
-	
+	myAdd m1;
+	cout << m1(10, 90) << endl;
 }
 
+//函数对象超出普通函数的概念，函数对象可以有自己的状态
+class myPrint
+{
+public:
+	myPrint()
+	{
+		this->count=0;
+	}
+	void operator()(string str)
+	{
+		cout << str << endl;
+		this->count++;
+	}
+	int count;//内部自己的状态
+};
+
+void test02()
+{
+	myPrint m;
+	m("chenxing");
+	m("chenxing");
+	m("chenxing");
+	m("chenxing");
+	cout<<"myprint调用次数" << m.count << endl;
+}
+
+//函数对象可以作为参数传递
+void doPrint(myPrint &mp,string str)
+{
+	mp(str);
+}
+void test03()
+{
+	myPrint myprint;
+	doPrint(myprint,"chenxing");
+}
 int main()
 {
-	test01();
+	//test01();
+	//test02();
+	test03();
 	system("pause");
 	return 0;
 }
