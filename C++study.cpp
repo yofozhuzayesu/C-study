46e398b8f0198ef869e3fd7fe002771c711f66d3
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-17 00:43:41 +0800

    Transform算法

--- C++study.cpp
+++ C++study.cpp
@@ -4,36 +4,25 @@
 using namespace std;
 
 /*
-	STL常用算法
-	算法主要是由头文件<algorothm><functional><numeric>组成
-	<algorithm>是所有头文件中最大的一个，范围涉及到比较、交换、查找、遍历操作、复制、修改等等
-	<numeric>体积很小，只包括几个在序列上面进行简单数学运算你的模板函数
-	<functional>定义了一些模板类，用以声明函数对象
+	遍历算法  搬运容器到另一个容器中
+	transform(iterator beg1,iterator end1,itetator beg2,_func)
+	_func  如果是普通函数的话就写函数名即可  是函数对象的话 需要写括号
 
 */
 
-/*
-	遍历算法  遍历容器元素
-	for_each(iterator beg,iterator end,_func)
-	三个参数分别表示 开始迭代器 结束迭代器  _func函数或者函数对象
-
-*/
-
-//普通函数
-void print01(int val)
+void myprint(int v)
 {
-	cout << val << " ";
+	cout << v << " ";
+	
 }
 
-//class对象
-class print02
+//搬运执行的相当是一个赋值操作，所以需要返回一个值
+int myTrans(int v)
 {
-public:
-	void operator()(int val)
-	{
-		cout << val << " ";
-	}
-};
+	return v;
+}
+
+
 void test01()
 {
 	vector<int>v;
@@ -41,12 +30,11 @@ void test01()
 	{
 		v.push_back(i);
 	}
-	//普通函数
-	for_each(v.begin(), v.end(),print01);
-
-	cout << endl;
-	//对象
-	for_each(v.begin(), v.end(), print02());
+	vector<int>v2;
+	//搬运之前需要先设置目标容器的容量 不然会崩
+	v2.resize(v.size());
+	transform(v.begin(), v.end(), v2.begin(), myTrans);
+	for_each(v2.begin(), v2.end(), myprint);
 }
 int main()
 {
