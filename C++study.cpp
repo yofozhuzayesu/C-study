b69d52d295964b372ecd3f03f3fc3fe11c968b6f
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-17 16:16:15 +0800

    copy-拷贝算法

--- C++study.cpp
+++ C++study.cpp
@@ -5,8 +5,9 @@
 using namespace std;
 
 /*
-	reverse 将容器内元素进行反转
-	reverse(iterator beg,iterator end)
+	copy 容器内指定范围的元素拷贝到另一容器中
+	copy(iterator beg,iterator end,iterator dest)
+	//dest 目标容器起始迭代器
 
 */
 
@@ -21,20 +22,16 @@ void test01()
 {
 
 	vector<int>v;
+	vector<int>vt;
 	v.push_back(15);
 	v.push_back(30);
 	v.push_back(18);
 	v.push_back(21);
 
-	cout << "反转之前的容器为：";
-	for_each(v.begin(), v.end(),myprint);
-	cout << endl;
-	
-	//反转容器
-	reverse(v.begin(), v.end());
-	cout << "反转之后的容器为：";
-	for_each(v.begin(), v.end(), myprint);
-	cout << endl;
+	//需要先给目标容器分配内存
+	vt.resize(v.size());
+	copy(v.begin(), v.end(), vt.begin());
+	for_each(vt.begin(), vt.end(), myprint);
 }
 
 int main()
