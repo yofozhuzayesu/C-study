0b6aca97e8a01cebc3e975faa444a20b018144c4
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-17 16:40:41 +0800

    算术生成算法-fill和accumulate

--- C++study.cpp
+++ C++study.cpp
@@ -1,13 +1,20 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-#include<string>
+#include<numeric>
 using namespace std;
 
 /*
-	swap 互换两个容器的元素  
-	swap(container c1,container c2)
-	交换容器时 注意两个容器类型要一致
+	常用算术生成算法
+	算术生成算法属于小型算法，使用时包含的头文件为 numeric
+
+	accmulate(iterator beg,iterator end,value)  计算容器元素累计总和
+	//value 是指的起始值  比如写0 就是0+v[1]+v[2]+v[3].....
+
+	fill(iterator beg,iterator end,value)  向容器中填充元素
+	//value填充的值
+	fill可以填充空容器（需要先定义容器容量）
+	也可以填充非空容器，相当于替换replace操作
 */
 
 //遍历输出
@@ -21,24 +28,16 @@ void test01()
 {
 
 	vector<int>v1;
-	vector<int>v2;
 	v1.push_back(15);
 	v1.push_back(20);
 	v1.push_back(18);
 	v1.push_back(20);
 	v1.push_back(19);
 	v1.push_back(21);
-
-	for (int i = 0; i < 10; i++)
-	{
-		v2.push_back(i);
-	}
-	swap(v1, v2);
-	for_each(v1.begin(), v1.end(), myprint);
-	cout << endl;
-	for_each(v2.begin(), v2.end(), myprint);
-	cout << endl;
+	//cout << accumulate(v1.begin(), v1.end(), 0) << endl;//113
 	
+	fill(v1.begin(), v1.begin() + 2, 100);
+	for_each(v1.begin(), v1.end(), myprint);
 }
 
 int main()
