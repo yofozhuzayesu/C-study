4d0d9236e6bb81f1cb4042588e29cd843cc7bd23
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-17 16:08:40 +0800

    merge-容器合并算法

--- C++study.cpp
+++ C++study.cpp
@@ -2,13 +2,13 @@
 #include<vector>
 #include<algorithm>
 #include<string>
-#include<time.h>
 using namespace std;
 
 /*
-	洗牌： 指定范围内的元素随机调整次序
-	random_shuffle(iterator beg,iterator end)
-	若想每次乱序结果不一样，同样需要设置一个随机数种子
+	merge 两个容器元素合并，并存储到另一容器中
+	merge(iterator beg1,iterator end1,iterator beg2,iterator end2,itetator dest)
+	//注意两个容器必须是有序的
+	//dest 目标容器开始迭代器
 */
 
 //遍历输出
@@ -17,74 +17,38 @@ void myprint(int v)
 	cout << v << "  ";
 }
 
-//降序谓词
-class mySort
-{
-public:
-	bool operator()(int v1, int v2)
-	{
-		return v1 > v2;
-	}
-};
 //内置数据类型
 void test01()
 {
 
 	vector<int>v;
+	vector<int>v2;
 	v.push_back(15);
 	v.push_back(30);
 	v.push_back(18);
 	v.push_back(21);
-	
-	//随机数种子
-	srand((unsigned int)time(NULL));
-	//随机打乱顺序
-	random_shuffle(v.begin(), v.end());
-	for_each(v.begin(), v.end(), myprint);
-	cout << endl;
 
-}
-
-//自定义数据类型
-class person
-{
-public:
-	person(string name, int age)
+	for (int i = 0; i < 10; i++)
 	{
-		this->m_name = name;
-		this->m_age = age;
+		//v.push_back(i + 1);
+		v2.push_back(i);
 	}
-	string m_name;
-	int m_age;
-};
-//遍历输出
-void myPrint2(const person &p)
-{
-	cout << "姓名：" << p.m_name << "年龄：" << p.m_age << endl;
+	
+	//目标容器
+	vector<int>vT;
+	//需要提前给目标容器给分配内存
+	vT.resize(v.size() + v2.size());
+	//需要给两个容器先排序 两个容器内的数据必须同为升序 或者降序
+	sort(v.begin(), v.end());
+	merge(v.begin(), v.end(), v2.begin(), v2.end(), vT.begin());
+	for_each(vT.begin(), vT.end(), myprint);
+	
+
 }
 
-void test02()
-{
-	vector<person> v;
-	person p1("张三", 19);
-	person p2("李四", 15);
-	person p3("王五", 26);
-	person p4("陈六", 35);
-	v.push_back(p1);
-	v.push_back(p2);
-	v.push_back(p3);
-	v.push_back(p4);
-	
-	//随机数种子
-	srand((unsigned int)time(NULL));
-	//随机打乱顺序
-	random_shuffle(v.begin(), v.end());
-	for_each(v.begin(), v.end(), myPrint2);
-}
 int main()
 {
-	//test01();
-	test02();
+	test01();
 	system("pause");
 	return 0;
 }
