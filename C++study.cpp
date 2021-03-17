68729cf0f1ca1d2b8039cf7981fdbf53e9285ad9
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-17 11:19:48 +0800

    查找指定元素是否存在-binary_search

--- C++study.cpp
+++ C++study.cpp
@@ -4,29 +4,30 @@
 using namespace std;
 
 /*
-	查找相邻重复元素
-	adjacent_find(iterator beg,iterator end)
-	//查找相邻重复元素，返回相邻元素的第一个位置的迭代器
+	查找指定元素是否存在
+	bool binary_search(iterator beg,iterator end,value)
+	//查找指定的元素，查到返回true，否则false
+	//注意：在无序序列中不可用
+
 */
 
 
-
-//查找内置数据
 void test01()
 {
 	vector<int>v;
 	v.push_back(10);
 	v.push_back(20);
-	v.push_back(20);
 	v.push_back(30);
-	vector<int>::iterator i = adjacent_find(v.begin(), v.end());
-	if (i!=v.end())
+	v.push_back(40);
+	//注意：容器必须是有序序列，如果是无序的话，会出错
+	bool b = binary_search(v.begin(), v.end(), 30);
+	if (b)
 	{
-		cout << "找到了元素" << *i << endl;
+		cout << "找到了" << endl;
 	}
 	else
 	{
-		cout << "未找到相邻的元素" << endl;
+		cout << "未找到" << endl;
 	}
 }
 
