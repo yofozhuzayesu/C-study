3ede78ed083697ad4aefe4d95e2a7b62b2da496f
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-17 11:42:09 +0800

    count_if算法

--- C++study.cpp
+++ C++study.cpp
@@ -5,20 +5,28 @@
 using namespace std;
 
 /*
-	统计元素出现的次数
-	count(iterator beg,iterator end,value)
+	按条件统计元素出现次数
+	count_if(iterator beg,iterator end,_Pred)
+	/_Pred谓词
 */
 
-
+class greaterFive
+{
+public:
+	bool operator()(int v)
+	{
+		return v > 5;
+	}
+};
 //内置数据类型
 void test01()
 {
 	vector<int>v;
-	v.push_back(10);
+	v.push_back(1);
 	v.push_back(10);
 	v.push_back(30);
 	v.push_back(40);
-	int i = count(v.begin(), v.end(), 10);
+	int i = count_if(v.begin(), v.end(), greaterFive());
 	cout << "出现了" << i << "次" << endl;
 
 }
@@ -32,20 +40,20 @@ public:
 		this->m_name = name;
 		this->m_age = age;
 	}
-	//参数里必须得加const 否则会报错
-	bool operator==(const person &p)
+	bool operator()(const person &p)
 	{
-		if (this->m_name ==p.m_name &&this->m_age==p.m_age)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		return p.m_age > 18;
 	}
 	string m_name;
 	int m_age;
+};
+class Greater18
+{
+public:
+	bool operator()(const person &p)
+	{
+		return p.m_age > 18;
+	}
 };
 void test02()
 {
@@ -56,11 +64,12 @@ void test02()
 	person p4("陈六", 35);
 	v.push_back(p1);
 	v.push_back(p2);
+	v.push_back(p2);
 	v.push_back(p3);
 	v.push_back(p4);
 
-	int i = count(v.begin(), v.end(), p2);
-	cout << "p2出现了" << i << "次" << endl;
+	int i = count_if(v.begin(), v.end(), Greater18());
+	cout << "年龄大于18的人有" << i << "人" << endl;
 }
 int main()
 {
