475a40a17fadc2500fa7663e0cbbd59591d139f8
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-17 16:58:04 +0800

    set_union 取并集算法

--- C++study.cpp
+++ C++study.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 /*
 	常用集合算法
-	set_intersection(iterator beg1,iterator end1,iterator beg2,iterator end2,interator dest)
-	//注意两个集合必须是有序序列
-	//dest目标容器开始迭代器
-	//返回一个迭代器，迭代器就是交集容器的end
+	set_union(iterator beg1,iterator end1,iterator beg2,iterator end2,iterator dest)
+	//求两个集合的并集
+	//两个集合必须是有序序列
+	//如果两个容器有相同的元素 只会取一个 
+	//但是一个容器内有相同的元素 会保留
 */
 
 //遍历输出
@@ -32,19 +33,19 @@ void test01()
 	v1.push_back(21);
 	sort(v1.begin(), v1.end());
 
-	for (int i = 0; i < 20; i++)
+	for (int i = 0; i < 16; i++)
 	{
 		v2.push_back(i);
 	}
 
-	//给目标容器开辟空间，最特殊的情况是 大容器包含小容器的全部元素 则令目标容器的容量为小容器大小
+	//给目标容器开辟空间，最特殊的情况是 两个容器没有交集，则目标容器的大小为两个容器大小相加
 	//使用min算法 同样也包含在algorithm头文件内
-	vt.resize(min(v1.size(), v2.size()));
-	vector<int>::iterator itend =  set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(), vt.begin());
+	vt.resize(v1.size()+ v2.size());
+	vector<int>::iterator itend =  set_union(v1.begin(), v1.end(), v2.begin(), v2.end(), vt.begin());
 	//如果不用一个itend接收的话，直接遍历到vt.end(),多余的部分还会被填充0
-	for_each(vt.begin(), vt.end(), myprint);//15 18 19 0 0 0
+	for_each(vt.begin(), vt.end(), myprint);
 	cout << endl;
-	for_each(vt.begin(), itend, myprint);// 15 18 19
+	for_each(vt.begin(), itend, myprint);
 	cout << endl;
 }
 
