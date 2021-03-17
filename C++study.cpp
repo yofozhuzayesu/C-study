fca5af6f6d75c699734deab97dba3d8dcc4f9e49
yofozhuzayesu <32996571+yofozhuzayesu@users.noreply.github.com>
2021-03-17 11:34:27 +0800

    count算法

--- C++study.cpp
+++ C++study.cpp
@@ -1,39 +1,71 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<string>
 using namespace std;
 
 /*
-	查找指定元素是否存在
-	bool binary_search(iterator beg,iterator end,value)
-	//查找指定的元素，查到返回true，否则false
-	//注意：在无序序列中不可用
-
+	统计元素出现的次数
+	count(iterator beg,iterator end,value)
 */
 
 
+//内置数据类型
 void test01()
 {
 	vector<int>v;
 	v.push_back(10);
-	v.push_back(20);
+	v.push_back(10);
 	v.push_back(30);
 	v.push_back(40);
-	//注意：容器必须是有序序列，如果是无序的话，会出错
-	bool b = binary_search(v.begin(), v.end(), 30);
-	if (b)
-	{
-		cout << "找到了" << endl;
-	}
-	else
-	{
-		cout << "未找到" << endl;
-	}
+	int i = count(v.begin(), v.end(), 10);
+	cout << "出现了" << i << "次" << endl;
+
 }
 
+//自定义数据类型
+class person
+{
+public:
+	person(string name, int age)
+	{
+		this->m_name = name;
+		this->m_age = age;
+	}
+	//参数里必须得加const 否则会报错
+	bool operator==(const person &p)
+	{
+		if (this->m_name ==p.m_name &&this->m_age==p.m_age)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	string m_name;
+	int m_age;
+};
+void test02()
+{
+	vector<person> v;
+	person p1("张三", 19);
+	person p2("李四", 15);
+	person p3("王五", 26);
+	person p4("陈六", 35);
+	v.push_back(p1);
+	v.push_back(p2);
+	v.push_back(p3);
+	v.push_back(p4);
+
+	int i = count(v.begin(), v.end(), p2);
+	cout << "p2出现了" << i << "次" << endl;
+}
 int main()
 {
-	test01();
+	//test01();
+	test02();
 	system("pause");
 	return 0;
 }
